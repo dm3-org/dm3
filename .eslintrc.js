f2b28e08b11e4d96629c165eaa3403f36638af20
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-02-01 14:12:28 +0100

    ci: init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+    },
+    extends: ['prettier'],
+    rules: {
+        'max-len': ['error', { code: 120 }],
+    },
+};
