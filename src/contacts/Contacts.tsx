80ffb604eaaddb88d09c8c73d942cc3352ccadb4
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-03-07 14:59:42 +0100

    refactor: split main css file into component css files

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ContactList from './ContactList';
 import AddContactForm from './AddContactForm';
 import * as Lib from '../lib';
+import './Contacts.css';
 
 interface ContactsProps {
     apiConnection: Lib.ApiConnection;
