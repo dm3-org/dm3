e255da93e6e20b81297dbe515a879cdca24a3f26
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-03-14 09:28:56 +0100

    feat: Support persisting user storage using file mode
    allow saving of message data
    
    user data file upload
    
    encrypt data file

--- src/storage/StorageView.tsx
+++ src/storage/StorageView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'react-chat-widget/lib/styles.css';
 import * as Lib from '../lib';
 import Icon from '../ui-shared/Icon';
@@ -9,38 +9,110 @@ interface StorageViewProps {
 }
 
 function StorageView(props: StorageViewProps) {
+    const downloadDbFile = (e: any) => {
+        e.preventDefault();
+        const blob = new Blob([JSON.stringify(Lib.sync(props.connection))], {
+            type: 'text/json',
+        });
+
+        const a = document.createElement('a');
+        a.download = 'export.json';
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvt = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
+        a.dispatchEvent(clickEvt);
+        a.remove();
+    };
+
+    const upload = (event: any) => {
+        if (event.target.files[0]) {
+            const file = event.target.files[0];
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                if (e.target) {
+                    Lib.load(
+                        props.connection,
+                        JSON.parse(e.target?.result as string),
+                    );
+                }
+            };
+            reader.readAsText(file);
+        }
+    };
+
+    const [showLoad, setShowLoad] = useState<boolean>(false);
+
     return (
         <div className="mt-auto w-100 ">
             <div className="row storage-view-container">
-                <div className="col-12 storage-view text-center brand">
-                    ENS Mail v0.0.2
+                <div className="col-12 storage-view text-center export-data">
+                    <div className="row">
+                        <div className="col-6">
+                            <button
+                                type="button"
+                                onClick={downloadDbFile}
+                                className={`w-100 btn btn-sm btn${
+                                    props.connection.db.synced
+                                        ? '-outline-secondary'
+                                        : '-primary'
+                                }`}
+                            >
+                                Save
+                            </button>
+                        </div>
+                        <div className="col-6">
+                            <button
+                                type="button"
+                                onClick={() => setShowLoad(!showLoad)}
+                                className="w-100 btn btn-sm btn-outline-secondary"
+                            >
+                                Load
+                            </button>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-12 storage-view">
-                    {props.connection.db.synced ? (
-                        <span
-                            className="badge bg-secondary text-dark outline-badge only-outline"
-                            title="Data synced"
-                        >
-                            <Icon iconClass="fas fa-database" />
-                            &nbsp;&nbsp;synced
-                        </span>
-                    ) : (
-                        <span
-                            className="badge bg-warning text-dark outline-badge"
-                            title="Data out of sync"
-                        >
-                            <Icon iconClass="fas fa-database" />
-                            &nbsp;&nbsp;out of sync
-                        </span>
+                    {showLoad && (
+                        <input
+                            type="file"
+                            className="form-control form-control-sm"
+                            onChange={(event) => upload(event)}
+                        />
                     )}
-                    &nbsp;
-                    <span
-                        className="badge bg-secondary text-dark outline-badge only-outline"
-                        title={`chainId: ${props.connection.provider.network.chainId}`}
-                    >
-                        <Icon iconClass="fas fa-network-wired" />
-                        &nbsp;&nbsp;{props.connection.provider.network.chainId}
-                    </span>
+                    {!showLoad && (
+                        <>
+                            {' '}
+                            {props.connection.db.synced ? (
+                                <span
+                                    className="badge bg-secondary text-dark outline-badge only-outline"
+                                    title="Data synced"
+                                >
+                                    <Icon iconClass="fas fa-database" />
+                                    &nbsp;&nbsp;synced
+                                </span>
+                            ) : (
+                                <span
+                                    className="badge bg-secondary text-dark outline-badge only-outline"
+                                    title="Data out of sync"
+                                >
+                                    <Icon iconClass="fas fa-database" />
+                                    &nbsp;&nbsp;out of sync
+                                </span>
+                            )}
+                            &nbsp;
+                            <span
+                                className="badge bg-secondary text-dark outline-badge only-outline"
+                                title={`chainId: ${props.connection.provider.network.chainId}`}
+                            >
+                                <Icon iconClass="fas fa-network-wired" />
+                                &nbsp;&nbsp;
+                                {props.connection.provider.network.chainId}
+                            </span>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
