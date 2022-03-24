ae1fa5f188976f1fabb9dfbf47f87c65f6cc392a
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-03-24 11:33:39 +0100

    feat: new sign in design

---
+++ src/sign-in/ChooseFile.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './SignIn.css';
+import * as Lib from '../lib';
+
+interface ChooseFileProps {
+    storageLocation: Lib.StorageLocation;
+    existingAccount: boolean;
+    setDataFile: (dataFile: string | undefined) => void;
+}
+
+function ChooseFile(props: ChooseFileProps) {
+    const upload = (event: any) => {
+        if (event.target.files[0]) {
+            const file = event.target.files[0];
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                if (e.target) {
+                    props.setDataFile(e.target?.result as string);
+                }
+            };
+            reader.readAsText(file);
+        }
+    };
+
+    if (
+        !(
+            props.existingAccount &&
+            props.storageLocation === Lib.StorageLocation.File
+        )
+    ) {
+        return null;
+    }
+
+    return (
+        <div className="row row-space">
+            <div className="col-md-4">
+                <input
+                    type="file"
+                    className="form-control"
+                    onChange={(event) => upload(event)}
+                />
+            </div>{' '}
+            <div className="col-md-8 help-text">
+                Connect an Ethereum account
+            </div>
+        </div>
+    );
+}
+
+export default ChooseFile;
---
+++ src/sign-in/ConnectButton.tsx
@@ -0,0 +1,118 @@
+import React, { useContext } from 'react';
+import './SignIn.css';
+import { ethers } from 'ethers';
+import Icon from '../ui-shared/Icon';
+import * as Lib from '../lib';
+import { GlobalContext } from '../GlobalContextProvider';
+import { EnsNameType } from '../reducers/EnsNames';
+
+import { ConnectionType } from '../reducers/Connection';
+
+interface ConnectButtonProps {
+    setExistingAccount: (exists: boolean) => void;
+}
+
+function ConnectButton(props: ConnectButtonProps) {
+    const { state, dispatch } = useContext(GlobalContext);
+
+    const connect = async () => {
+        dispatch({
+            type: ConnectionType.ChangeConnectionState,
+            payload: Lib.ConnectionState.WaitingForAccountConntection,
+        });
+
+        const accountConnection = await Lib.connectAccount(
+            state.connection.provider!,
+        );
+
+        props.setExistingAccount(accountConnection.existingAccount);
+
+        if (accountConnection.account) {
+            dispatch({
+                type: ConnectionType.ChangeAccount,
+                payload: {
+                    address: accountConnection.account,
+                },
+            });
+            dispatch({
+                type: ConnectionType.ChangeConnectionState,
+                payload: accountConnection.connectionState,
+            });
+
+            const ensName = await Lib.lookupAddress(
+                state.connection.provider as ethers.providers.JsonRpcProvider,
+                accountConnection.account,
+            );
+            if (ensName) {
+                dispatch({
+                    type: EnsNameType.AddEnsName,
+                    payload: {
+                        address: accountConnection.account,
+                        name: ensName,
+                    },
+                });
+            }
+        } else {
+            dispatch({
+                type: ConnectionType.ChangeConnectionState,
+                payload: accountConnection.connectionState,
+            });
+        }
+    };
+
+    const getConnectionIconClass = (connectionState: Lib.ConnectionState) => {
+        switch (connectionState) {
+            case Lib.ConnectionState.AccountConnectionRejected:
+                return <Icon iconClass="fas fa-exclamation-circle" />;
+
+            case Lib.ConnectionState.WaitingForAccountConntection:
+                return <Icon iconClass="fas fa-spinner fa-spin" />;
+
+            case Lib.ConnectionState.SignInFailed:
+            case Lib.ConnectionState.SignedIn:
+            case Lib.ConnectionState.WaitingForSignIn:
+            case Lib.ConnectionState.SignInReady:
+                return <Icon iconClass="fas fa-check-circle" />;
+
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div className="row">
+            <div className="col-md-4">
+                <button
+                    onClick={connect}
+                    type="button"
+                    className={`btn btn-${
+                        state.connection.connectionState ===
+                        Lib.ConnectionState.AccountConnectionRejected
+                            ? 'danger'
+                            : 'primary'
+                    } btn-lg w-100`}
+                    disabled={
+                        !(
+                            state.connection.connectionState ===
+                                Lib.ConnectionState.AccountConntectReady ||
+                            state.connection.connectionState ===
+                                Lib.ConnectionState.AccountConnectionRejected
+                        )
+                    }
+                >
+                    Connect Account
+                    <span className="push-end">
+                        {getConnectionIconClass(
+                            state.connection.connectionState,
+                        )}
+                    </span>
+                </button>
+            </div>
+            <div className="col-md-8 help-text">
+                Connect an Ethereum account
+            </div>
+        </div>
+    );
+}
+
+export default ConnectButton;
