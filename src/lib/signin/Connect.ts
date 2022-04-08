5b56349b284b1f6b086cca25936cf2824b30657b
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-04-08 15:15:38 +0200

    feat: add profile signature check

--- src/lib/signin/Connect.ts
+++ src/lib/signin/Connect.ts
@@ -1,5 +1,9 @@
 import { ethers } from 'ethers';
-import { ProfileRegistryEntry } from '../account/Account';
+import { check } from 'prettier';
+import {
+    checkProfileRegistryEntry,
+    ProfileRegistryEntry,
+} from '../account/Account';
 import { GetProfileRegistryEntry } from '../external-apis/BackendAPI';
 import { RequestAccounts } from '../external-apis/InjectedWeb3API';
 import { ConnectionState } from '../web3-provider/Web3Provider';
@@ -15,10 +19,20 @@ export async function connectAccount(
 }> {
     try {
         const account = await requestAccounts(provider);
+        const profile = await getProfileRegistryEntry(account);
+        if (
+            profile &&
+            !checkProfileRegistryEntry(
+                profile?.profileRegistryEntry,
+                profile?.signature,
+                account,
+            )
+        ) {
+            throw Error('Profile signature is invalid');
+        }
         return {
             account,
-            existingAccount: (await getProfileRegistryEntry(account))
-                ?.profileRegistryEntry.publicKeys
+            existingAccount: profile?.profileRegistryEntry.publicKeys
                 ? true
                 : false,
             connectionState: ConnectionState.CollectingSignInData,
