467d496d9de903bc82a208bc722cfc0e7fee7a9a
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-06-20 15:03:13 +0200

    feat: standalone web app

--- packages/ens-mail-web/src/App.tsx
+++ packages/ens-mail-web/src/App.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 //@ts-ignore
 import EnsMail from 'ens-mail-react';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <EnsMail />
+        <div className="container">
+            <EnsMail inline={true} />
         </div>
     );
 }
