3e49d55fea66eea859da91adab7f83cb9c913695
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-07-01 16:32:10 +0200

    feat: web app background animation

--- packages/ens-mail-react/src/index.tsx
+++ packages/ens-mail-react/src/index.tsx
@@ -2,11 +2,11 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import EnsMail from './EnsMail';
 import './index.css';
-
 import GlobalContextProvider from './GlobalContextProvider';
 import { Config, getConfig } from './utils/Config';
+export * as DarkLogo from './logos/DarkLogo';
 
-export { StorageLocation } from 'ens-mail-lib';
+export { StorageLocation, ConnectionState } from 'ens-mail-lib';
 
 function index(props: Partial<Config>) {
     return (
