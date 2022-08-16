aaeb664053fe347c82f94445ef7d50a121471444
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-08-16 09:14:22 +0200

    feat: include legal documents

--- packages/react/src/logos/DarkLogo.tsx
+++ packages/react/src/logos/DarkLogo.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 
 import Logo from '../assets/dm3_Signet_blue-white.svg';
+import './DarkLogo.css';
 
 function DarkLogo() {
     return (
@@ -8,21 +9,29 @@ function DarkLogo() {
             style={{ fontSize: '35px', filter: 'brightness(90%)' }}
             className="d-flex"
         >
-            <img
-                style={{
-                    width: '50px',
-                }}
-                src={Logo}
-                className="align-self-center"
-            />
-            <div className="align-self-center">
-                <span
-                    className="ms-3"
-                    style={{ fontWeight: '600', color: '#fff' }}
-                >
-                    dm
-                </span>
-                <span style={{ fontWeight: '400', color: '#fff' }}>3</span>
+            <a href="https://dm3.network">
+                <img
+                    style={{
+                        width: '50px',
+                    }}
+                    src={Logo}
+                    className="align-self-center"
+                />
+            </a>
+            <div className="align-self-center ms-3">
+                <div style={{ fontSize: '12px ' }} className="text-end">
+                    <span className="badge bg-warning text-dark">beta</span>
+                </div>
+                <div>
+                    <a href="https://dm3.network" className="logo-link">
+                        <span style={{ fontWeight: '600', color: '#fff' }}>
+                            dm
+                        </span>
+                        <span style={{ fontWeight: '400', color: '#fff' }}>
+                            3
+                        </span>
+                    </a>
+                </div>
             </div>
         </div>
     );
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -60,6 +60,27 @@ function App() {
                     </a>
                 )}
             </div>
+            <nav
+                className="navbar fixed-bottom navbar-light "
+                style={{ backgroundColor: '#000000ff !important' }}
+            >
+                <div className="container-fluid text-center ">
+                    <div className="w-100">
+                        <a
+                            className="text-muted legal"
+                            href="https://dm3.network/privacy-policy/"
+                        >
+                            Privacy Policy
+                        </a>
+                        <a
+                            className="text-muted legal ms-4"
+                            href="https://dm3.network/terms-and-conditions/"
+                        >
+                            Terms & Conditions
+                        </a>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 }
