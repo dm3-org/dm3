c6ebb64efec38451b397b06075c3df59ca795ec0
Alex <alex.plutta@googlemail.com>
2022-11-01 14:58:11 +0100

    implement handleSubmitMessage

--- packages/backend/src/rpc-proxy.ts
+++ packages/backend/src/rpc-proxy.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Axios } from 'axios';
 import 'dotenv/config';
 import * as Lib from 'dm3-lib/dist.backend';
+import { RedisPrefix } from './redis';
 
 const DM3_SUBMIT_MESSAGE = 'dm3_submitMessage';
 
@@ -14,7 +15,7 @@ export default (axios: Axios) => {
             return res.send(400);
         }
 
-        const { method } = req.body;
+        const { method, params } = req.body;
         if (method === DM3_SUBMIT_MESSAGE) {
             return handleSubmitMessage(req, res, next);
         }
@@ -23,12 +24,50 @@ export default (axios: Axios) => {
     return router;
 };
 
-const handleSubmitMessage = (
+const handleSubmitMessage = async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction,
 ) => {
-    res.send(200);
+    const {
+        params: [envelop, token],
+    } = req.body;
+
+    if (!envelop || !token) {
+        return res.send(400);
+    }
+
+    try {
+        await Lib.Delivery.incomingMessage(
+            { envelop, token },
+            req.app.locals.deliveryServicePrivateKey,
+            req.app.locals.loadSession,
+            async (conversationId: string, envelop: Lib.EncryptionEnvelop) => {
+                if (req.app.locals.redisClient) {
+                    await req.app.locals.redisClient.zAdd(
+                        RedisPrefix.Conversation + conversationId,
+                        {
+                            score: new Date().getTime(),
+                            value: JSON.stringify(envelop),
+                        },
+                    );
+                } else {
+                    throw Error('db not connected');
+                }
+            },
+            (socketId: string, envelop: Lib.EncryptionEnvelop) => {
+                req.app.locals.io.sockets.to(socketId).emit('message', envelop);
+            },
+        );
+        res.send(200);
+    } catch (error) {
+        req.app.locals.logger.warn({
+            method: 'RPC SUBMIT MESSAGE',
+            error,
+        });
+
+        return res.send(400);
+    }
 };
 
 const forwardToRpcNode =
@@ -39,20 +78,6 @@ const forwardToRpcNode =
         next: express.NextFunction,
     ) => {
         try {
-            const envelop = '';
-            const token = '';
-
-            const deliveryServiceProfile = '';
-            const getSession = (address: string) => {};
-            const storeNewMessage = () => {};
-            const send = () => {};
-            /*  Lib.Delivery.incomingMessage(
-            { envelop, token },
-            deliveryServiceProfile,
-            getSession,
-            storeNewMessage,
-        ); */
-
             const data = (await axios.post(process.env.RPC as string, req.body))
                 .data;
             console.log('data ' + data);
