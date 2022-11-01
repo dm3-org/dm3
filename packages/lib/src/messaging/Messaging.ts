0dfd10201078cc9d0fcf2978f95bd782a482c3d0
Alex <alex.plutta@googlemail.com>
2022-11-01 15:52:05 +0100

    apply new message structure

--- packages/lib/src/messaging/Messaging.ts
+++ packages/lib/src/messaging/Messaging.ts
@@ -35,9 +35,10 @@ export type MessageType =
     | 'NEW'
     | 'DELETE_REQUEST'
     | 'EDIT'
-    | 'THREAD_POST'
+    | 'REPLY'
     | 'REACTION'
-    | 'READ_RECEIPT';
+    | 'READ_RECEIPT'
+    | 'RESEND_REQUEST';
 
 export interface Attachment {
     type: string;
