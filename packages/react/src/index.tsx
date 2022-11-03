08f7b85b90a15841c87b6b880901f4149548bcfa
hai-ko <mail@heiko-burkhardt.de>
2022-11-03 11:02:52 +0100

    ci: run build in ci (#324)

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,6 @@
 import 'react-app-polyfill/stable';
 import React from 'react';
-import DM3 from './Dm3';
+import DM3App from './Dm3';
 import './index.css';
 import GlobalContextProvider from './GlobalContextProvider';
 import { Config, getConfig } from './utils/Config';
@@ -9,14 +9,12 @@ import * as Lib from 'dm3-lib';
 export * as DarkLogo from './logos/DarkLogo';
 export const ConnectionState = Lib.web3provider.ConnectionState;
 
-function index(props: Partial<Config>) {
+export function DM3(props: Partial<Config>) {
     return (
         <div className="entry">
             <GlobalContextProvider>
-                <DM3 config={getConfig(props)} />
+                <DM3App config={getConfig(props)} />
             </GlobalContextProvider>
         </div>
     );
 }
-
-export default index;
