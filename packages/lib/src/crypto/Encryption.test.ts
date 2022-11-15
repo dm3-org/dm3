06a8d8b5af0e9a8afb23da34d5479f8fc90e12d0
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-11-15 08:25:01 +0100

    feat: padding for encryption

--- packages/lib/src/crypto/Encryption.test.ts
+++ packages/lib/src/crypto/Encryption.test.ts
@@ -6,6 +6,8 @@ import {
     EncryptedPayload,
 } from './Encryption';
 
+import * as Encrypted from './Encryption.test.json';
+
 const keyA = {
     publicKey:
         '0xc75df5b29bd9657ea60d322789a50de4ddbba2bf1796596f6ac5dbf66c86bd74',
@@ -50,13 +52,8 @@ test('should encrypt to same ciphertext with same nonce', async () => {
     expect(encryptedB).toEqual(encryptedA);
 });
 
-test('should dencrypt correctly', async () => {
-    const encrypted: EncryptedPayload = {
-        ciphertext: '0xbcf6f9fc6754dc9a76be49dea394e2afa737cbb7',
-        nonce: '0x2ee06c9483bba6eefabfecc9',
-    };
-
-    expect(await decrypt(keyB, encrypted)).toEqual('test');
+test('should decrypt correctly', async () => {
+    expect(await decrypt(keyB, Encrypted.testDataA)).toEqual('test');
 });
 
 test('should encrypt a string asymetric', async () => {
@@ -72,14 +69,7 @@ test('should encrypt a string asymetric', async () => {
 });
 
 test('should decrypt asymetric', async () => {
-    const payload: EncryptedPayload = {
-        ciphertext: '0x7a3108d2c144a7c623508963036c3aaa0cc1fa3a',
-        nonce: '0xd5ac94e1b63aee44cd11c233',
-        ephemPublicKey:
-            '0x1e1460a0ed4291ae2eae124c4f49e85f10eb242069b88593cddfd31b3f5ca107',
-    };
-
-    const decrypted = await decryptAsymmetric(keyA, payload);
+    const decrypted = await decryptAsymmetric(keyA, Encrypted.testDataB);
     expect(decrypted).toEqual('test');
 });
 
