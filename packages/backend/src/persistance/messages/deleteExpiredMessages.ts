5e3c681e14c7b0eda94c1422893ecb7f7926efb9
Alex <alex.plutta@googlemail.com>
2022-11-16 18:10:49 +0100

    add deleteExpiredMessages

---
+++ packages/backend/src/persistance/messages/deleteExpiredMessages.ts
@@ -0,0 +1,21 @@
+import * as Lib from 'dm3-lib/dist.backend';
+import { RedisPrefix } from '../../redis';
+import { Redis } from '../getDatabase';
+
+export function deleteExpiredMessages(
+    redisClient: Redis,
+): (time: number) => Promise<void> {
+    return async (time: number) => {
+        const conversions = await redisClient.keys(
+            RedisPrefix.Conversation + '*',
+        );
+
+        const now = new Date().getTime();
+
+        await Promise.all(
+            conversions.map((conversion) =>
+                redisClient.zRemRangeByScore(conversion, time, now),
+            ),
+        );
+    };
+}
