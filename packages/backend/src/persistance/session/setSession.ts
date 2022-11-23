f8f0e092a2883040515308d155cbc91c847f3f56
Alex <alex.plutta@googlemail.com>
2022-11-23 11:57:21 +0100

    add setSession to getDatabase

---
+++ packages/backend/src/persistance/session/setSession.ts
@@ -0,0 +1,20 @@
+import { RedisPrefix } from '../../redis';
+import { Redis } from '../getDatabase';
+import * as Lib from 'dm3-lib/dist.backend';
+
+export function setSession(redis: Redis) {
+    return async (account: string, session: Lib.delivery.Session) => {
+        const isValid = Lib.validateSchema(
+            Lib.delivery.schema.SessionSchema,
+            session,
+        );
+
+        if (!isValid) {
+            throw Error('Invalid session');
+        }
+        await redis.set(
+            RedisPrefix.Session + Lib.external.formatAddress(account),
+            Lib.stringify(session),
+        );
+    };
+}
