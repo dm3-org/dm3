7511cb51251e265849fd2958a8f6608b08b6ba6b
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-11-29 14:03:43 +0100

    feat: support data URIs

--- packages/lib/src/account/profileResolver/ProfileResolver.ts
+++ packages/lib/src/account/profileResolver/ProfileResolver.ts
@@ -7,5 +7,5 @@ export type ProfileResolver<T extends Dm3Profile> = {
     //Determines if the certain resolver is capable of resolving the according textRecord
     isProfile: (textRecord: string) => boolean;
     //resolves a textRecord to a userProfile if isProfile evaluates to true
-    resolveProfile: (textRecord: string) => Promise<T | undefined>;
+    resolveProfile: (textRecord: string) => Promise<T>;
 };
