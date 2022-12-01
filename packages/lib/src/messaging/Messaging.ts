1a56ec296b0d2b96b7807be7a7e19fab9c04dd2f
Alex <alex.plutta@googlemail.com>
2022-12-01 15:02:36 +0100

    fetch messages from all delivery services (#356)
    feat: 356 - fetch messages from all delivery services

--- packages/lib/src/messaging/Messaging.ts
+++ packages/lib/src/messaging/Messaging.ts
@@ -13,6 +13,8 @@ import {
     UserDB,
 } from '../storage/Storage';
 import { Connection } from '../web3-provider/Web3Provider';
+import { getDeliveryServiceProfile } from '../delivery';
+import axios from 'axios';
 
 export interface Message {
     to: string;
@@ -198,10 +200,45 @@ export async function getMessages(
     storeMessages: (envelops: StorageEnvelopContainer[]) => void,
     userDb: UserDB,
 ): Promise<StorageEnvelopContainer[]> {
+    const profile = connection.account?.profile;
+
+    if (!profile) {
+        throw 'Account has no profile';
+    }
+    //Fetch evey delivery service's profie
+    const deliveryServices = await Promise.all(
+        profile.deliveryServices.map(async (ds) => {
+            const deliveryServiceProfile = await getDeliveryServiceProfile(
+                ds,
+                connection,
+                async (url) => (await axios.get(url)).data,
+            );
+            return deliveryServiceProfile?.url;
+        }),
+    );
+
+    //Filter every deliveryService without an url
+    const deliveryServiceUrls = deliveryServices.filter(
+        (ds): ds is string => !!ds,
+    );
+
+    //Fetch messages from each deliveryService
+    const messages = await Promise.all(
+        deliveryServiceUrls.map(async (baseUrl) => {
+            return await getNewMessages(
+                connection,
+                deliveryServiceToken,
+                contact,
+                baseUrl,
+            );
+        }),
+    );
+
+    //Flatten the message arrays of each delivery service to one message array
+    const allMessages = messages.reduce((agg, cur) => [...agg, ...cur]);
+
     const envelops = await Promise.all(
-        (
-            await getNewMessages(connection, deliveryServiceToken, contact)
-        ).map(async (envelop): Promise<StorageEnvelopContainer> => {
+        allMessages.map(async (envelop): Promise<StorageEnvelopContainer> => {
             const decryptedEnvelop = await decryptMessages([envelop], userDb);
             const decryptedPostmark = JSON.parse(
                 await decryptAsymmetric(
