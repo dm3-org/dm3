383f1a41a0b547899fdb8aadcad0345b8d301531
Heiko Burkhardt <mail@heiko-burkhardt.de>
2022-12-20 09:59:21 +0100

    feat: auth with sigining key

--- packages/lib/src/session/getSessionFromStorage.ts
+++ packages/lib/src/session/getSessionFromStorage.ts
@@ -1,19 +1,11 @@
-import { PersonalSign } from '../external-apis/InjectedWeb3API';
+import { ProfileKeys } from '../account';
 import { load } from '../storage';
 import { ConnectionState } from '../web3-provider';
-import { Connection } from '../web3-provider/Web3Provider';
-import { signProfileKeyPair } from './SignIn/signProfileKeyPair';
-
-const DEFAULT_NONCE = 0;
 
 export async function getSessionFromStorage(
-    connection: Partial<Connection>,
-    personalSign: PersonalSign,
     storageFile: string,
-    nonce: number = DEFAULT_NONCE,
+    keys: ProfileKeys,
 ) {
-    const keys = await signProfileKeyPair(connection!, personalSign, nonce);
-
     const externalData = await load(
         JSON.parse(storageFile),
         keys.storageEncryptionKey,
