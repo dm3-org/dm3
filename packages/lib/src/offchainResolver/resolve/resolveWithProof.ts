8e90a790e26f593632c17f719ec1455c0ca6bbf3
Alex <alex.plutta@googlemail.com>
2023-01-09 16:58:30 +0100

    add description to resolveWithProof

--- packages/lib/src/offchainResolver/resolve/resolveWithProof.ts
+++ packages/lib/src/offchainResolver/resolve/resolveWithProof.ts
@@ -3,6 +3,15 @@ import { log, stringify } from '../..';
 import { getResolverInterface } from './getResolverInterface';
 import { ResolveResponse } from './types';
 
+/**
+ * This funcition can by used by a client to ensure onchain that the {@see UserProfile} returned by the ciip gateway
+ * is valid and was signed by a certain signer specified at the OffchainResolver contract
+ * @param provider {@see ether.BaseProvider}
+ * @param resolverAddress the address of the OffchainResolver Smart contract.
+ * @param request the calldata returned be the resolved method of the OffchainResolver Smart contract
+ * @param response {@see ResolveResponse}
+ * @returns See resolved {@see UserProfile} if sucesfully validated onchain
+ */
 export async function resolveWithProof(
     provider: ethers.providers.BaseProvider,
     resolverAddress: string,
@@ -16,12 +25,18 @@ export async function resolveWithProof(
             getResolverInterface(),
             provider,
         );
-        //Get the interface of an ResolverContract capalble of resolving text records
-        const iface = getResolverInterface();
-        const profileHash = iface.encodeFunctionResult('text(bytes32,string)', [
-            stringify(userProfile),
-        ]);
 
+        const textResolver = getResolverInterface();
+
+        const profileHash = textResolver.encodeFunctionResult(
+            'text(bytes32,string)',
+            [stringify(userProfile)],
+        );
+
+        /**
+         * The response has to be compiled the same way as in SignatureVerifier.verify
+         * Otherwise {@see resolveWithProof} will fail
+         */
         const response = ethers.utils.defaultAbiCoder.encode(
             ['bytes', 'uint64', 'bytes'],
             [profileHash, validUntil, sig],
@@ -32,7 +47,7 @@ export async function resolveWithProof(
             request,
         );
 
-        const [result] = iface.decodeFunctionResult('text', resolverRes);
+        const [result] = textResolver.decodeFunctionResult('text', resolverRes);
 
         return result;
     } catch (e) {
