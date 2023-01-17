ad7ca71b9277f2b20c499230e6f103d32c32f7d5
Alex <alex.plutta@googlemail.com>
2023-01-17 13:02:53 +0100

    399 ccpi eth account method (#405)
    feat: 399 ccpi eth account method (#405)

---
+++ packages/offchain-resolver/src/persistance/profile/getUserProfileByAddress.test.ts
@@ -0,0 +1,28 @@
+import { getRedisClient, Redis, getDatabase } from '../getDatabase';
+import { IDatabase } from '../IDatabase';
+const SENDER_ADDRESS = '0x25A643B6e52864d0eD816F1E43c0CF49C83B8292';
+import { getUserProfileByAddress } from './getUserProfileByAddress';
+const { expect } = require('chai');
+
+describe('getUserProfileByAddress', () => {
+    let redisClient: Redis;
+    let db: IDatabase;
+
+    beforeEach(async () => {
+        redisClient = await getRedisClient();
+        db = await getDatabase(redisClient);
+        await redisClient.flushDb();
+    });
+
+    afterEach(async () => {
+        await redisClient.flushDb();
+        await redisClient.disconnect();
+    });
+    it('Retuns null if there is no profile for the address', async () => {
+        const profile = await getUserProfileByAddress(redisClient)(
+            SENDER_ADDRESS,
+        );
+
+        expect(profile).to.be.null;
+    });
+});
--- packages/offchain-resolver/src/persistance/profile/index.ts
+++ packages/offchain-resolver/src/persistance/profile/index.ts
@@ -1,6 +1,6 @@
 export { getUserProfile } from './getUserProfile';
 export { setUserProfile } from './setUserProfile';
 export { hasAddressProfile } from './hasAddressProfile';
-
+export { getUserProfileByAddress } from './getUserProfileByAddress';
 export const USER_PROFILE_KEY = 'USER_PROFILE:';
 export const ADDRESS_TO_PROFILE_KEY = 'ADDRESS_TO_PROFILE_KEY:';
