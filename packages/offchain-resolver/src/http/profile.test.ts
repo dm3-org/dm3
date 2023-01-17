cd9bce8515847ea350d779d7ca1c268a3d391d5d
Alex <alex.plutta@googlemail.com>
2023-01-17 10:48:27 +0100

    add address endpoint

--- packages/offchain-resolver/src/http/profile.test.ts
+++ packages/offchain-resolver/src/http/profile.test.ts
@@ -30,7 +30,7 @@ describe('Profile', () => {
         await redisClient.flushDb();
         await redisClient.disconnect();
     });
-    describe('Store UserProfile', () => {
+    describe('Store UserProfile by ens name', () => {
         it('Rejects invalid schema', async () => {
             const { status, body } = await request(app).post(`/name`).send({
                 name: 'foo.dm3.eth',
@@ -171,6 +171,28 @@ describe('Profile', () => {
                 'address has already claimed a subdomain',
             );
         });
+        it('Rejects if name has the address format', async () => {
+            const { signer, profile, signature } = await getSignedUserProfile();
+
+            //Fund wallet so their balance is not zero
+            const [wale] = await hreEthers.getSigners();
+            await wale.sendTransaction({
+                to: signer,
+                value: hreEthers.BigNumber.from(1),
+            });
+
+            const { status, body } = await request(app).post(`/name`).send({
+                name: signer,
+                address: signer,
+                signedUserProfile: {
+                    signature,
+                    profile,
+                },
+            });
+
+            expect(status).to.equal(400);
+            expect(body.error).to.equal('Invalid ENS name');
+        });
         it('Stores a valid profile', async () => {
             const { signer, profile, signature } = await getSignedUserProfile();
 
@@ -183,6 +205,147 @@ describe('Profile', () => {
 
             const { status } = await request(app).post(`/name`).send({
                 name: 'foo.dm3.eth',
+                address: signer,
+                signedUserProfile: {
+                    signature,
+                    profile,
+                },
+            });
+
+            expect(status).to.equal(200);
+        });
+    });
+    describe('Store UserProfile by address', () => {
+        it('Rejects invalid schema', async () => {
+            const { status, body } = await request(app).post(`/address`).send({
+                address: SENDER_ADDRESS,
+                signedUserProfile: {},
+            });
+
+            expect(status).to.equal(400);
+            expect(body.error).to.equal('invalid schema');
+        });
+        it('Rejects invalid profile', async () => {
+            const profile: Lib.account.UserProfile = {
+                publicSigningKey:
+                    '0ekgI3CBw2iXNXudRdBQHiOaMpG9bvq9Jse26dButug=',
+                publicEncryptionKey:
+                    'Vrd/eTAk/jZb/w5L408yDjOO5upNFDGdt0lyWRjfBEk=',
+                deliveryServices: [''],
+            };
+
+            const wallet = hreEthers.Wallet.createRandom();
+
+            const signature = await wallet.signMessage('foo');
+
+            const { status, body } = await request(app).post(`/address`).send({
+                address: wallet.address,
+                signedUserProfile: {
+                    profile,
+                    signature,
+                },
+            });
+
+            expect(status).to.equal(400);
+            expect(body.error).to.equal('invalid profile');
+        });
+
+        it('Rejects if subdomain has already a profile', async () => {
+            const profile2: Lib.account.UserProfile = {
+                publicSigningKey: '',
+                publicEncryptionKey: '',
+                deliveryServices: [''],
+            };
+
+            const offChainProfile1 = await getSignedUserProfile();
+            const offChainProfile2 = await getSignedUserProfile(profile2);
+
+            //Fund wallets so their balance is not zero
+            const [wale] = await hreEthers.getSigners();
+            await wale.sendTransaction({
+                to: offChainProfile1.signer,
+                value: hreEthers.BigNumber.from(1),
+            });
+            await wale.sendTransaction({
+                to: offChainProfile2.signer,
+                value: hreEthers.BigNumber.from(1),
+            });
+
+            const res1 = await request(app)
+                .post(`/address`)
+                .send({
+                    address: offChainProfile1.signer,
+                    signedUserProfile: {
+                        signature: offChainProfile1.signature,
+                        profile: offChainProfile1.profile,
+                    },
+                });
+
+            expect(res1.status).to.equal(200);
+
+            const res2 = await request(app)
+                .post(`/address`)
+                .send({
+                    address: offChainProfile1.signer,
+                    signedUserProfile: {
+                        signature: offChainProfile1.signature,
+                        profile: offChainProfile1.profile,
+                    },
+                });
+
+            expect(res2.status).to.equal(400);
+            expect(res2.body.error).to.eql(
+                'address has already claimed a subdomain',
+            );
+        });
+        it('Rejects if address already claimed a subdomain', async () => {
+            const offChainProfile1 = await getSignedUserProfile();
+
+            //Fund wallets so their balance is not zero
+            const [wale] = await hreEthers.getSigners();
+            await wale.sendTransaction({
+                to: offChainProfile1.signer,
+                value: hreEthers.BigNumber.from(1),
+            });
+
+            const res1 = await request(app)
+                .post(`/address`)
+                .send({
+                    address: offChainProfile1.signer,
+                    signedUserProfile: {
+                        signature: offChainProfile1.signature,
+                        profile: offChainProfile1.profile,
+                    },
+                });
+
+            expect(res1.status).to.equal(200);
+
+            const res2 = await request(app)
+                .post(`/address`)
+                .send({
+                    address: offChainProfile1.signer,
+                    signedUserProfile: {
+                        signature: offChainProfile1.signature,
+                        profile: offChainProfile1.profile,
+                    },
+                });
+
+            expect(res2.status).to.equal(400);
+            expect(res2.body.error).to.eql(
+                'address has already claimed a subdomain',
+            );
+        });
+        it('Stores a valid profile', async () => {
+            const { signer, profile, signature } = await getSignedUserProfile();
+
+            //Fund wallet so their balance is not zero
+            const [wale] = await hreEthers.getSigners();
+            await wale.sendTransaction({
+                to: signer,
+                value: hreEthers.BigNumber.from(1),
+            });
+
+            const { status } = await request(app).post(`/address`).send({
                 address: signer,
                 signedUserProfile: {
                     signature,
--- packages/offchain-resolver/src/http/profile.ts
+++ packages/offchain-resolver/src/http/profile.ts
@@ -40,6 +40,15 @@ export function profile(web3Provider: ethers.providers.BaseProvider) {
                     .status(400)
                     .send({ error: 'address has already claimed a subdomain' });
             }
+
+            //The /address endpoint has to be used to create an subdomain based on a address
+            const nameIsAddress = ethers.utils.isAddress(name.split('.')[0]);
+
+            if (nameIsAddress) {
+                return res.status(400).send({
+                    error: 'Invalid ENS name',
+                });
+            }
             const sendersBalance = await web3Provider.getBalance(address);
 
             //To avoid spam the user is required to have at least a non-zero balance
@@ -56,6 +65,60 @@ export function profile(web3Provider: ethers.providers.BaseProvider) {
                     .status(400)
                     .send({ error: 'subdomain already claimed' });
             }
+            await req.app.locals.db.setUserProfile(
+                name,
+                signedUserProfile.profile,
+                address,
+            );
+
+            return res.send(200);
+        },
+    );
+    router.post(
+        '/address',
+        async (req: express.Request & { app: WithLocals }, res, next) => {
+            const { signedUserProfile, address } = req.body;
+            const isSchemaValid = Lib.validateSchema(
+                Lib.account.schema.SignedUserProfile,
+                signedUserProfile,
+            );
+
+            //Check if schema is valid
+            if (!isSchemaValid) {
+                return res.status(400).send({ error: 'invalid schema' });
+            }
+
+            const profileIsValid = Lib.account.checkUserProfile(
+                signedUserProfile,
+                address,
+            );
+
+            //Check if profile sig is correcet
+            if (!profileIsValid) {
+                return res.status(400).send({ error: 'invalid profile' });
+            }
+
+            const hasAddressProfile = await req.app.locals.db.hasAddressProfile(
+                address,
+            );
+
+            //One address can only claim one subdomain
+            if (hasAddressProfile) {
+                return res
+                    .status(400)
+                    .send({ error: 'address has already claimed a subdomain' });
+            }
+
+            const name = `${address}.dm3.eth`;
+
+            const profileExists = await req.app.locals.db.getUserProfile(name);
+
+            if (profileExists) {
+                return res
+                    .status(400)
+                    .send({ error: 'subdomain already claimed' });
+            }
+
             await req.app.locals.db.setUserProfile(
                 name,
                 signedUserProfile.profile,
