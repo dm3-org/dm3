29050a13d184987d5752ae350840e82dce604e78
Heiko Burkhardt <mail@heiko-burkhardt.de>
2023-03-02 11:39:46 +0100

    feat: hide and unhide contacts

--- packages/react/src/contacts/ContactList.tsx
+++ packages/react/src/contacts/ContactList.tsx
@@ -3,17 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import ContactListEntry from './ContractListEntry';
 import { GlobalContext } from '../GlobalContextProvider';
+import * as Lib from 'dm3-lib';
 
 function ContactList() {
     const { state } = useContext(GlobalContext);
     const contactsList = state.accounts.contacts
-        ? state.accounts.contacts.map((contact) => (
-              <ContactListEntry
-                  key={contact.account.ensName}
-                  connection={state.connection}
-                  contact={contact}
-              />
-          ))
+        ? state.accounts.contacts
+              .filter(
+                  (contact) =>
+                      !state.userDb?.hiddenContacts.find(
+                          (hiddenContact) =>
+                              Lib.account.normalizeEnsName(hiddenContact) ===
+                              Lib.account.normalizeEnsName(
+                                  contact.account.ensName,
+                              ),
+                      ),
+              )
+              .map((contact) => (
+                  <ContactListEntry
+                      key={contact.account.ensName}
+                      connection={state.connection}
+                      contact={contact}
+                  />
+              ))
         : [];
 
     return <div className="list-group">{contactsList}</div>;
