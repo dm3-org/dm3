e5c7dd2f1d877b62dede70eb6abb048c671fc1a8
Alex <alex.plutta@googlemail.com>
2023-03-08 09:48:42 -0500

    fix crypto import

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -2,7 +2,7 @@ export type { Connection } from './web3-provider/Web3Provider';
 
 export * as delivery from './delivery';
 export * as account from './account/src';
-export * as crypto from './crypto';
+export * as crypto from './crypto/src';
 export * as external from './external-apis';
 export * as messaging from './messaging';
 export * as offchainResolver from './offchainResolver';
