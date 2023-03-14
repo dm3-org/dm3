14528943b3837ac74344370abf709859e254a375
Alex <alex.plutta@googlemail.com>
2023-03-13 20:09:50 -0500

    fix deliveryServiceProperties schema

--- packages/lib/src/delivery/schema/index.ts
+++ packages/lib/src/delivery/schema/index.ts
@@ -4,8 +4,9 @@ import DeliveryServicePropertiesSchema from './DeliveryServiceProperties.schema.
 import MessageSubmissionSchema from './MessageSubmission.schema.json';
 import SessionSchema from './Session.schema.json';
 
-export const Acknoledgment = AcknoledgmentSchema;
+export const Acknoledgment = AcknoledgmentSchema.definitions.Acknoledgment;
 export const DeliveryServiceProfile = DeliveryServiceProfileSchema;
-export const DeliveryServiceProperties = DeliveryServicePropertiesSchema;
+export const DeliveryServiceProperties =
+    DeliveryServicePropertiesSchema.definitions.DeliveryServiceProperties;
 export const MessageSubmission = MessageSubmissionSchema;
 export const Session = SessionSchema;
