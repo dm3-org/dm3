cf38a653f2d383472da5165483bb84c0e32ff20e
Alex <alex.plutta@googlemail.com>
2023-03-14 11:30:11 -0500

    delete old deliverServieProfileExport

--- packages/lib/src/delivery/schema/index.ts
+++ packages/lib/src/delivery/schema/index.ts
@@ -1,11 +1,9 @@
 import AcknoledgmentSchema from './Acknoledgment.schema.json';
-import DeliveryServiceProfileSchema from './DeliveryServiceProfile.schema.json';
 import DeliveryServicePropertiesSchema from './DeliveryServiceProperties.schema.json';
 import MessageSubmissionSchema from './MessageSubmission.schema.json';
 import SessionSchema from './Session.schema.json';
 
 export const Acknoledgment = AcknoledgmentSchema.definitions.Acknoledgment;
-export const DeliveryServiceProfile = DeliveryServiceProfileSchema;
 export const DeliveryServiceProperties =
     DeliveryServicePropertiesSchema.definitions.DeliveryServiceProperties;
 export const MessageSubmission = MessageSubmissionSchema;
