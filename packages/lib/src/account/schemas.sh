75612addcee2058b16ea4c3639f7ecf02b895b2a
Alex <alex.plutta@googlemail.com>
2023-03-14 11:20:44 -0500

    update schema build script

--- packages/lib/src/account/schemas.sh
+++ packages/lib/src/account/schemas.sh
@@ -1,2 +1,3 @@
-yarn ts-json-schema-generator -f tsconfig.json --path Account.ts --type SignedUserProfile -o ./schema/SignedUserProfile.schema.json --no-type-check \
-&& yarn ts-json-schema-generator -f tsconfig.json --path ./ProfileExtension/ProfileExtension.ts --type ProfileExtension -o ./schema/ProfileExtension.schema.json --no-type-check
+yarn ts-json-schema-generator -f tsconfig.json --path Account.ts --type SignedUserProfile -o ./src/schema/SignedUserProfile.schema.json --no-type-check \
+&& yarn ts-json-schema-generator -f tsconfig.json --path ./ProfileExtension/ProfileExtension.ts --type ProfileExtension -o ./src/schema/ProfileExtension.schema.json --no-type-check \
+&& yarn ts-json-schema-generator -f tsconfig.json --path ./deliveryServiceProfile/Delivery.ts --type DeliveryServiceProfile -o ./src/schema/DeliveryServiceProfile.schema.json --no-type-check \
