e9337e5db65ebddd0d5bd0964348b3d03c067d23
Alex <alex.plutta@googlemail.com>
2023-03-15 20:51:16 -0500

    add messaging to lib index

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -4,7 +4,7 @@ export * as delivery from 'dm3-lib-delivery';
 export * as account from 'dm3-lib-account';
 export * as crypto from 'dm3-lib-crypto';
 export * as external from './external-apis';
-export * as messaging from './messaging';
+export * as messaging from 'dm3-lib-messaging';
 export * as offchainResolver from './offchainResolver';
 export * as session from './session';
 export * as storage from './storage';
