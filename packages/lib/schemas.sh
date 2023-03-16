f98169ac4e2bc8c82fb8998c5241b01590774a79
Alex <alex.plutta@googlemail.com>
2023-03-15 19:40:34 -0500

    dont build the lib schema anymore

--- packages/lib/schemas.sh
+++
@@ -1,3 +0,0 @@
-yarn ts-json-schema-generator -f tsconfig.json --path src/lib/Messaging.ts --type Message -o src/schema.json --no-type-check \
-&& yarn ts-json-schema-generator -f tsconfig.json --path lib/src/messaging/Messaging.ts --type EncryptionEnvelop -o src/messaging/schema/EncryptionEnvelop.schema.json --no-type-check -i EncryptionEnvelop.schema.json\
-
