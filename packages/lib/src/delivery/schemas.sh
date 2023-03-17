d0c457a71d48245deb546011bdaa95765c58a97b
Alex <alex.plutta@googlemail.com>
2023-03-16 19:05:49 -0500

    fix delivery schemas.sh

--- packages/lib/src/delivery/schemas.sh
+++ packages/lib/src/delivery/schemas.sh
@@ -1,5 +1,5 @@
-yarn ts-json-schema-generator -f tsconfig.json --path Delivery.ts --type DeliveryServiceProperties -o ./schema/DeliveryServiceProperties.schema.json --no-type-check \
-&& yarn ts-json-schema-generator -f tsconfig.json --path Messages.ts --type Acknoledgment -o ./schema/Acknoledgment.schema.json --no-type-check \
-&& yarn ts-json-schema-generator -f tsconfig.json --path Messages.ts --type MessageSubmission -o ./schema/MessageSubmission.schema.json --no-type-check \
-&& yarn ts-json-schema-generator -f tsconfig.json --path Session.ts --type Session -o ./schema/Session.schema.json --no-type-check \
+yarn ts-json-schema-generator -f tsconfig.json --path Delivery.ts --type DeliveryServiceProperties -o ./src/schema/DeliveryServiceProperties.schema.json --no-type-check \
+&& yarn ts-json-schema-generator -f tsconfig.json --path Messages.ts --type Acknoledgment -o ./src/schema/Acknoledgment.schema.json --no-type-check \
+&& yarn ts-json-schema-generator -f tsconfig.json --path Messages.ts --type MessageSubmission -o ./src/schema/MessageSubmission.schema.json --no-type-check \
+&& yarn ts-json-schema-generator -f tsconfig.json --path Session.ts --type Session -o ./src/schema/Session.schema.json --no-type-check \
 
