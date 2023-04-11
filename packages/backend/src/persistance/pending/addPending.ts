4c5e477a2f94ac1404e37dbc9507417dbfa49262
Alex <alex.plutta@googlemail.com>
2023-04-11 16:28:58 +0200

    rename account imports from account to profile

--- packages/backend/src/persistance/pending/addPending.ts
+++ packages/backend/src/persistance/pending/addPending.ts
@@ -1,5 +1,6 @@
 import { Redis, RedisPrefix } from '../getDatabase';
 import * as Lib from 'dm3-lib/dist.backend';
+import { getIdEnsName } from '../session/getIdEnsName';
 
 export function addPending(redis: Redis) {
     return async (ensName: string, contactEnsName: string): Promise<void> => {
