5bfd456f61c271f499475af6a279a28fb3443a52
Alex <alex.plutta@googlemail.com>
2023-04-17 13:43:52 +0200

    476 add fetch all messages to ds (#480)

--- packages/backend/src/persistance/messages/createMessage.ts
+++ packages/backend/src/persistance/messages/createMessage.ts
@@ -20,5 +20,27 @@ export function createMessage(redis: Redis) {
             score: createdAt,
             value: Lib.stringify(envelop),
         });
+
+        /**
+         * add a redis set key = envelop.metadata.deliveryInformation.to and value = conversationId
+         */
+        /**
+         * We can assume that the deliveryInformation is always encrypted because the
+         * DS must've encrypted it before persisting the message to the database.
+         *
+         *
+         *  In the future we have to refactor the DeliveryInformation Type
+         *  to we can ensure that on compile time. https://github.com/corpus-io/dm3/issues/479
+         */
+        const encryptedDeliverInformation = envelop.metadata
+            .deliveryInformation as Lib.messaging.DeliveryInformation;
+
+        await redis.zAdd(
+            RedisPrefix.IncomingConversations + encryptedDeliverInformation.to,
+            {
+                score: createdAt,
+                value: conversationId,
+            },
+        );
     };
 }
