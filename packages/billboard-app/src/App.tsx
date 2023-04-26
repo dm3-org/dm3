085575a0334b86008c6d8d5d5a50495c3089d1b8
Karl Adler <karl@Karls-MBP.fritz.box>
2023-04-26 17:54:14 +0200

    initial react setup and dm3-billboard-api fix

---
+++ packages/billboard-app/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { log } from 'dm3-lib-shared';
+import { getBillboardApiClient } from 'dm3-lib-billboard-api';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
