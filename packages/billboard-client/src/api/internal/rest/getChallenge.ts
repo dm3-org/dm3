61a75052012bf8fc3411c88dda0e4f6471c85a62
AlexNi245 <alex.plutta@googlemail.com>
2023-05-02 20:00:24 +0200

    add rest method to create session and get new token

---
+++ packages/billboard-client/src/api/internal/rest/getChallenge.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { normalizeEnsName } from 'dm3-lib-profile';
+import { log } from 'dm3-lib-shared';
+
+export async function getChallenge(
+    dsUrl: string,
+    ensName: string,
+): Promise<string | null> {
+    const url = `${dsUrl}auth/${normalizeEnsName(ensName)}`;
+
+    try {
+        const { data } = await axios.get(url);
+        return data.challenge;
+    } catch (e) {
+        log("can't get challenge from ds " + dsUrl);
+        return null;
+    }
+}
---
+++ packages/billboard-client/src/api/internal/rest/getNewToken.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { normalizeEnsName } from 'dm3-lib-profile';
+import { log } from 'dm3-lib-shared';
+
+export async function getNewToken(
+    dsUrl: string,
+    ensName: string,
+    signature: string,
+): Promise<string | null> {
+    const url = `${dsUrl}auth/${normalizeEnsName(ensName)}`;
+    try {
+        const { data } = await axios.post(url, { signature });
+        return data.token;
+    } catch (e) {
+        log("can't get new token from ds");
+        return null;
+    }
+}
