f02e9706f3fdf65f34decd204f626ac3fb773755
Heiko Burkhardt <mail@heiko-burkhardt.de>
2023-05-11 08:59:00 +0200

    feat: profile wildcards for resolver

--- packages/offchain-resolver-backend/src/http/handleCcipRequest/handler/resolveText.ts
+++ packages/offchain-resolver-backend/src/http/handleCcipRequest/handler/resolveText.ts
@@ -1,12 +1,19 @@
 import { IDatabase } from '../../../persistance/IDatabase';
 import { PROFILE_RECORD_NAME } from 'dm3-lib-profile/dist.backend';
 import { stringify } from 'dm3-lib-shared/dist.backend';
+import { interceptTextRecord } from './intercept';
 
 export async function handleText(db: IDatabase, request: any) {
     const { record, name } = request;
 
     if (record !== PROFILE_RECORD_NAME) {
         throw Error(`${record} Record is not supported by this resolver`);
+    }
+
+    const interceptResult = interceptTextRecord(name, record);
+
+    if (interceptResult) {
+        return interceptResult;
     }
 
     const userProfile = await db.getUserProfile(name);
