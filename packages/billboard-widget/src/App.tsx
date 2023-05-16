7fd41ff3798435d8a8b36bc37314013aa977f2cb
Karl Adler <karl@Karls-MBP.fritz.box>
2023-05-17 00:52:57 +0200

    add interface docs and update README

--- packages/billboard-widget/src/App.tsx
+++ packages/billboard-widget/src/App.tsx
@@ -17,15 +17,46 @@ import './styles/app.pcss';
 import { AuthProps } from './hooks/useAuth';
 
 export interface BillboardWidgetProps {
+    /** A StaticJsonRpcProvider */
     web3Provider: ethers.providers.StaticJsonRpcProvider;
+    /** common billboard widget options */
     options?: {
+        /** Custom class name added to the main component div */
         className?: string;
-        withToBottomButton?: boolean;
+        /** A custom url for all user avatars or a function that takes the users
+         * identifier and returns the url. `(identifier: string) => url`
+         **/
         avatarSrc?: string | ((hash?: string) => string);
     };
+    /** Config for the billboard message fetching part.
+     *  - mockedApi: boolean; if you don't have a client yet you can use a mocked version.
+     *  - billboardId: string;
+     *  - fetchSince: Date;
+     *  - limit: number;
+     *  - websocketUrl: string;
+     **/
     clientOptions?: ClientProps;
+    /**
+     * Auth options are required to submit a new message using your dm3 profile.
+     * - deliveryServiceUrl: string;
+     * - offchainResolverUrl: string;
+     * - siweAddress: string;
+     * - siweSig: string;
+     */
     authOptions?: AuthProps;
+    /** Fine tune your auto scrolling
+     * - withToBottomButton?: boolean; Whether to display a Scroll-to-Bottom
+     * - behavior?: 'smooth' | 'auto';
+     * - containerClassName?: string;
+     */
     scrollOptions?: ContainerProps;
+    /**
+     * Branding options to customize the default DM3 branding logos etc.
+     * - logoImageSrc?: string; small logo displayed always on top.
+     * - emptyMessagesImageSrc?: string; displayed when no messages received.
+     * - slogan?: string; displayed next to small logo on top.
+     * - emptyViewText?: string; A small text display when no messages received.
+     */
     branding?: {
         logoImageSrc?: string;
         emptyMessagesImageSrc?: string;
