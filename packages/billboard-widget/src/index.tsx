07c6a048edf75243caee62ab74138caae6454eff
Karl Adler <906623+karladler@users.noreply.github.com>
2023-05-30 21:09:21 +0200

    add separate css build (#507)
    Co-authored-by: Karl Adler <karl@Karls-MacBook-Pro.local>

--- packages/billboard-widget/src/index.tsx
+++ packages/billboard-widget/src/index.tsx
@@ -1,7 +1,8 @@
 import App from './App';
 import { BillboardWidgetProps, ClientProps } from './types';
 import 'normalize.css';
-import './styles/app.pcss';
+// import './styles/app.pcss';
+import './styles/classic.pcss';
 
 export function Dm3Widget(props: BillboardWidgetProps) {
     return (
