cb015a3d2722910bbee69684c4542e48dbd997a1
Karl Adler <906623+karladler@users.noreply.github.com>
2023-05-30 21:09:21 +0200

    add separate css build (#507)
    Co-authored-by: Karl Adler <karl@Karls-MacBook-Pro.local>

---
+++ packages/billboard-widget/buildCss.js
@@ -0,0 +1,19 @@
+const esbuild = require('esbuild');
+
+async function build() {
+    await esbuild.build({
+        entryPoints: ['src/styles/app.pcss', 'src/styles/classic.pcss'],
+        outdir: 'dist/styles/',
+        minify: true,
+        bundle: true,
+        loader: {
+            '.css': 'css',
+            '.pcss': 'css',
+        },
+    });
+}
+
+build().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
