41f120258a5282d76583b08b96ca44d44849c518
Alex <alex.plutta@googlemail.com>
2023-05-31 16:48:27 +0200

    Billboard widget enhancements (#509)
    * add viewerCount polling
    
    * widget demo showcases siwe
    
    * widgetDemo supports new siwe message
    
    * use dark dm3 logo
    
    * add messages to state
    
    * increase version to 0.2.6
    
    * format vite config

--- packages/billboard-widget/vite.config.ts
+++ packages/billboard-widget/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
-        plugins: [react(), dts()],
+        plugins: [react(), dts(), nodePolyfills()],
         define: {
             'process.env': {},
         },
