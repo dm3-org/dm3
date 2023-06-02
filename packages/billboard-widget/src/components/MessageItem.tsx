0b6961bde9d222d23cbb43742552c573b947f5e4
AlexNi245 <alex.plutta@googlemail.com>
2023-06-02 12:02:58 +0200

    show users message in a different color

--- packages/billboard-widget/src/components/MessageItem.tsx
+++ packages/billboard-widget/src/components/MessageItem.tsx
@@ -1,5 +1,7 @@
 import { Message } from 'dm3-lib-messaging';
 
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 import Avatar from './Avatar';
 import MessageFooter from './MessageFooter';
 
@@ -11,12 +13,18 @@ interface Props {
 
 function MessageItem(props: Props) {
     const { message } = props;
+    const { ensName } = useContext(AuthContext);
+
+    const messageStyle =
+        'content text-sm ' +
+        ' ' +
+        (message.metadata.from === ensName ? 'own' : '');
 
     return (
         <div className="item-container">
             <Avatar identifier={message.metadata.from} />
-            <div className="message">
-                <div className="content text-sm">{message.message}</div>
+            <div className="message ">
+                <div className={messageStyle}>{message.message}</div>
                 <MessageFooter {...message.metadata} />
             </div>
         </div>
