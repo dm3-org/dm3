ab23075689065f4bc0ba39a1303866f02a4fb449
Alex <alex.plutta@googlemail.com>
2023-06-07 16:27:32 +0200

    Fix sender avatar + address (#519)
    * make avatarSource fn async
    
    * release v 0.2.11
    
    * fix avatar + name
    
    * fix avatar + name
    
    * lint
    
    * optimize imports

--- packages/billboard-widget/src/components/CreateMessage.tsx
+++ packages/billboard-widget/src/components/CreateMessage.tsx
@@ -1,11 +1,10 @@
-import { ethers } from 'ethers';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import gearIcon from '../assets/gear-icon.svg';
 
 import { AuthContext } from '../context/AuthContext';
+import { GlobalContext } from '../context/GlobalContext';
 import Avatar from './Avatar';
 import ButtonWithTimer from './ButtonWithTimer/ButtonWithTimer';
-import { GlobalContext } from '../context/GlobalContext';
 interface Props {
     onClickSettings?: () => void;
     onCreateMsg: (msg: string) => void;
@@ -19,18 +18,29 @@ function CreateMessage(props: Props) {
     const [textAreaContent, setTextAreaContent] = useState('');
     const { options } = useContext(GlobalContext);
 
+    const [userName, setUserName] = useState('');
+    useEffect(() => {
+        const resolveUserName = async () => {
+            if (typeof options?.userNameResolver === 'function') {
+                const resolvedUserName = await options.userNameResolver(
+                    ensName,
+                );
+                setUserName(resolvedUserName);
+            } else {
+                setUserName(ensName);
+            }
+        };
+
+        resolveUserName();
+    }, [ensName, options]);
     return (
         <div className="create-message">
             <div className="container">
-                <Avatar
-                    identifier={ethers.utils.keccak256(
-                        ethers.utils.toUtf8Bytes(ensName),
-                    )}
-                />
+                <Avatar identifier={ensName} />
                 <div className="message-create-area">
                     <div className="create-header">
                         <div className="info text-xxs">
-                            {`Logged in as ${ensName}`}
+                            {`Logged in as ${userName}`}
                         </div>
                         {typeof onClickSettings === 'function' ? (
                             <button className="settings-button">
