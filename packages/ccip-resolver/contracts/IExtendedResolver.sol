1d8d91db7cae9bcbf5a83c268f5ad2fb219cca80
Heiko Burkhardt <mail@heiko-burkhardt.de>
2023-06-28 08:36:36 +0200

    refactor: remove ccip resolver from this repo

--- packages/ccip-resolver/contracts/IExtendedResolver.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.4;
-
-interface IExtendedResolver {
-    function resolve(bytes memory name, bytes memory data)
-        external
-        view
-        returns (bytes memory);
-}
--- packages/ccip-resolver/src/utils/readKeyEnv.ts
+++
@@ -1,8 +0,0 @@
-export function readKeyFromEnv(keyName: string) {
-    const key = process.env[keyName];
-    if (!key) {
-        throw Error(`Missing ${keyName} in env`);
-    }
-
-    return key;
-}
