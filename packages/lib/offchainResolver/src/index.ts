0fa267c0445395a96305b83012748c8f413d0290
Heiko Burkhardt <mail@heiko-burkhardt.de>
2023-06-28 09:00:16 +0200

    test: seperate tests in ci

--- packages/lib/offchainResolver/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { decodeRequest } from './encoding/decodeRequest';
-export { encodeResponse } from './encoding/encodeResponse';
-export { decodeDnsName } from './dns/decodeDnsName';
-export { encodeEnsName } from './dns/encodeEnsName';
-
-export type { DecodedCcipRequest } from './types';
