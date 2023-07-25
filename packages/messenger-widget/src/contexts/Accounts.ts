d23c8a83a5b618ff0be0a498cf243d6faee8ed2d
Bhupesh-MS <bhupeshd@mindfiresolutions.com>
2023-07-25 18:46:16 +0530

    fixed styles check

---
+++ packages/messenger-widget/src/contexts/Accounts.ts
@@ -0,0 +1,63 @@
+import { log } from 'dm3-lib-shared';
+import {
+    Accounts,
+    AccountsActions,
+    AccountsType,
+} from '../utils/enum-type-utils';
+import { normalizeEnsName } from 'dm3-lib-profile';
+
+export function accountsReducer(state: Accounts, action: AccountsActions) {
+    switch (action.type) {
+        case AccountsType.SetSelectedContact:
+            if (state.selectedContact === action.payload?.account.ensName) {
+                return state;
+            } else {
+                log(
+                    `[Accounts] Set selected account to ${action.payload?.account.ensName}`,
+                    'info',
+                );
+
+                return {
+                    ...state,
+                    selectedContact: action.payload && {
+                        ...action.payload,
+                        account: {
+                            ...action.payload.account,
+                            ensName: normalizeEnsName(
+                                action.payload.account.ensName,
+                            ),
+                        },
+                    },
+                };
+            }
+
+        case AccountsType.SetContacts:
+            log(
+                `[Accounts] Set ${
+                    action.payload ? action.payload.length : '0'
+                } contacts`,
+                'info',
+            );
+            return {
+                ...state,
+                contacts: action.payload,
+            };
+
+        case AccountsType.SetAccountInfoView:
+            if (state.accountInfoView === action.payload) {
+                return state;
+            } else {
+                log(
+                    `[Accounts] Set account info view ${action.payload}`,
+                    'info',
+                );
+                return {
+                    ...state,
+                    accountInfoView: action.payload,
+                };
+            }
+
+        default:
+            return state;
+    }
+}
