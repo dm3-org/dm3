3ca502bf5b12175eb1c97d10a9c38c77ab78a41e
Bhupesh-MS <bhupeshd@mindfiresolutions.com>
2023-07-25 18:46:16 +0530

    fixed styles check

--- packages/messenger-widget/src/declaration.d.ts
+++ packages/messenger-widget/src/declaration.d.ts
@@ -1,8 +1,9 @@
-declare module "*.png";
-declare module "*.gif";
-declare module "*.jpg";
+// To support images with extensions png, jpg and gif
+declare module '*.png';
+declare module '*.gif';
+declare module '*.jpg';
 
-declare module "localforage" {
-  let localforage: LocalForage;
-  export = localforage;
+declare module 'localforage' {
+    let localforage: LocalForage;
+    export = localforage;
 }
