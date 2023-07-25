0427a22a0773828307f60fa847b3a4664569b20d
Bhupesh-MS <bhupeshd@mindfiresolutions.com>
2023-07-26 01:17:52 +0530

    stashed files added

---
+++ packages/messenger-widget/src/interfaces/context.ts
@@ -0,0 +1,40 @@
+import { Account, DeliveryServiceProfile } from 'dm3-lib-profile';
+import { SelectedRightView } from '../utils/enum-type-utils';
+
+export interface Contact {
+    account: Account;
+    deliveryServiceProfile?: DeliveryServiceProfile;
+}
+
+export interface AuthState {
+    allSessions: { [address: string]: AuthSession };
+    currentSession?: AuthSession;
+    recentlyUsedSession?: string;
+}
+
+export interface AuthSession {
+    storage: string;
+    token: string;
+    ensName: string;
+    storageEncryptionKey?: string;
+}
+
+export interface Cache {
+    abis: Map<string, string>;
+    avatarUrls: Map<string, string>;
+}
+
+export interface UiState {
+    showAddContact: boolean;
+    selectedRightView: SelectedRightView;
+    maxLeftView: boolean;
+    show: boolean;
+    lastMessagePull: number;
+    proflieExists: boolean;
+    browserStorageBackup: boolean;
+    showContactList: boolean;
+}
+
+export interface GlobalContextProviderProps {
+    children: JSX.Element;
+}
