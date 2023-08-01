8cca20d9ff7327f1ed7dd81d27a7f57e8f54d407
Bhupesh-mfsi <137804674+Bhupesh-mfsi@users.noreply.github.com>
2023-08-01 13:51:50 +0530

    548 walletconnect (#561)
    * stashed files added
    
    * signin page ui
    
    * renamed file
    
    * fixed linting issues
    
    * integrated rainbow wallet
    
    * integrated metamask wallet & fixed home image strech issue
    
    * fixed error message
    
    * removed provider check
    
    * removed fontawesome library & added svg image
    
    ---------
    
    Co-authored-by: Bhupesh-MS <bhupeshd@mindfiresolutions.com>

--- packages/messenger-widget/src/index.tsx
+++ packages/messenger-widget/src/index.tsx
@@ -3,7 +3,6 @@ import './common.css';
 import 'react-app-polyfill/stable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Home } from './views/Home/Home';
 import { setTheme } from './utils/style-utils';
 import { Config } from './interfaces/config';
