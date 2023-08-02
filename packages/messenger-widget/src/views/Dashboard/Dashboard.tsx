57d3eee15616eeb7d68b7b2a0d9d4ecac0ca23aa
Bhupesh-mfsi <137804674+Bhupesh-mfsi@users.noreply.github.com>
2023-08-02 14:37:43 +0530

    557 user profile (#562)
    * stashed files added
    
    * signin page ui
    
    * renamed file
    
    * fixed linting issues
    
    * integrated rainbow wallet
    
    * integrated metamask wallet & fixed home image strech issue
    
    * fixed error message
    
    * removed provider check
    
    * removed fontawesome library & added svg image
    
    * added user profile page
    
    ---------
    
    Co-authored-by: Bhupesh-MS <bhupeshd@mindfiresolutions.com>

--- packages/messenger-widget/src/views/Dashboard/Dashboard.tsx
+++ packages/messenger-widget/src/views/Dashboard/Dashboard.tsx
@@ -1,13 +1,31 @@
-import Storage from '../../components/Storage/Storage';
 import './Dashboard.css';
+import { useContext } from 'react';
+import LeftView from '../../components/LeftView/LeftView';
+import RightView from '../../components/RightView/RightView';
+import Storage from '../../components/Storage/Storage';
+import { DashboardProps } from '../../interfaces/props';
+import { GlobalContext } from '../../utils/context-utils';
 
-export default function Dashboard() {
+export default function Dashboard(props: DashboardProps) {
+    const { state } = useContext(GlobalContext);
+
     return (
-        <div className="dashboard">
+        <div className="h-auto">
             <Storage />
-            <div className="row m-0 height-fill">
-                <div className="col-lg-3 col-md-3 col-sm-12 p-0"></div>
-                <div className="col-lg-9 col-md-9 col-sm-12 p-0 h-auto"></div>
+            <div className="row m-0 h-auto">
+                <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                    <LeftView />
+                </div>
+                <div
+                    className={
+                        'col-lg-9 col-md-9 col-sm-12 p-0 h-auto' +
+                        (state.rightView.showProfile
+                            ? ' dashboard-right-view-highlight'
+                            : '')
+                    }
+                >
+                    <RightView />
+                </div>
             </div>
         </div>
     );
