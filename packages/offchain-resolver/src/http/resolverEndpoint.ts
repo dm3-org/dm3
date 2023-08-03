0c98d4d2d81b70c800cd0b4b0107263c645a3865
Heiko Burkhardt <mail@heiko-burkhardt.de>
2023-08-03 13:14:24 +0200

    build: docker compose

--- packages/offchain-resolver/src/http/resolverEndpoint.ts
+++ packages/offchain-resolver/src/http/resolverEndpoint.ts
@@ -34,7 +34,7 @@ export function resolverEndpoint() {
 
                     res.status(404).send({ message: 'Record not found' });
                 } else {
-                    res.send({ response });
+                    res.send(response);
                 }
             } catch (e) {
                 req.app.locals.logger.warn((e as Error).message);
