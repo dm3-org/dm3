cd647050aca43f0e44d08b145d191eb710e9f4d1
Heiko Burkhardt <mail@heiko-burkhardt.de>
2023-08-03 13:14:24 +0200

    build: docker compose

--- packages/offchain-resolver/src/http/handleCcipRequest/encoding/getResolverInterface.ts
+++ packages/offchain-resolver/src/http/handleCcipRequest/encoding/getResolverInterface.ts
@@ -2,8 +2,11 @@ import { ethers } from 'ethers';
 
 export function getResolverInterface() {
     return new ethers.utils.Interface([
-        'function resolve(bytes calldata name, bytes calldata data) external view returns(bytes)',
+        // eslint-disable-next-line max-len
+        'function resolveWithContext(bytes calldata name,bytes calldata data,bytes calldata context) external view returns (bytes memory result)',
+        //Text
         'function text(bytes32 node, string calldata key) external view returns (string memory)',
+        //Address
         'function addr(bytes32 node) external view returns (address)',
     ]);
 }
