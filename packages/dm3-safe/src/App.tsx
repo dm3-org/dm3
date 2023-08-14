73d3561d0a0640d2b7929b3d3d3697830613e1d9
AlexNi245 <alex.plutta@googlemail.com>
2023-08-14 11:07:41 +0200

    fetch owners from safe

--- packages/dm3-safe/src/App.tsx
+++ packages/dm3-safe/src/App.tsx
@@ -1,13 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
+import { useEffect, useState } from 'react';
+
 //@ts-ignore
 import { DM3 } from 'dm3-react';
 
 function App() {
+    const { sdk, safe } = useSafeAppsSDK();
+    sdk.safe.getEnvironmentInfo();
+
+    useEffect(() => {
+        safe.owners.forEach((owner) => {
+            const ensName = `${owner}${process.env.REACT_APP_ADDR_ENS_SUBDOMAIN}`;
+            setOwnersEnsNames((ownersEnsNames) => [...ownersEnsNames, ensName]);
+        });
+    }, []);
+
+    const [ownersEnsNames, setOwnersEnsNames] = useState<string[]>([]);
+
     return (
         <>
             <DM3
-                defaultContacts={['help.dm3.eth']}
+                defaultContacts={['help.dm3.eth', ...ownersEnsNames]}
                 defaultServiceUrl={process.env.REACT_APP_DEFAULT_SERVICE}
                 showAlways={true}
                 // connectionStateChange={(state: ConnectionState) =>
