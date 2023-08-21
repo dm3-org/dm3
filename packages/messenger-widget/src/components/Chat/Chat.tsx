7f783d508a3dbeab3c39bff5aaeb16450cb7d76f
Bhupesh-MS <bhupeshd@mindfiresolutions.com>
2023-08-21 18:27:25 +0530

    profile not configured info box

--- packages/messenger-widget/src/components/Chat/Chat.tsx
+++ packages/messenger-widget/src/components/Chat/Chat.tsx
@@ -1,20 +1,24 @@
 import './Chat.css';
 import { useContext } from 'react';
 import { GlobalContext } from '../../utils/context-utils';
+import ConfigProfileAlertBox from '../ContactProfileAlertBox/ContactProfileAlertBox';
 
 export function Chat() {
     const { state } = useContext(GlobalContext);
 
     return (
         <div
-            className={'text-primary-color'.concat(
-                ' ',
+            className={
                 state.accounts.selectedContact
                     ? 'highlight-chat-border'
-                    : 'highlight-chat-border-none',
-            )}
+                    : 'highlight-chat-border-none'
+            }
         >
-            Chat screen...
+            {/* To show information box that contact has not created profile */}
+            {!state.accounts.selectedContact?.account.profile
+                ?.publicEncryptionKey && <ConfigProfileAlertBox />}
+
+            <div className="mt-3 ms-2 text-primary-color">Chat screen...</div>
         </div>
     );
 }
