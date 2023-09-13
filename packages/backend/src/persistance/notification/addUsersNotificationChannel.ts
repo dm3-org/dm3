db2c7dda0099f8b383cb441eeaff73651797bb8f
AlexNi245 <alex.plutta@googlemail.com>
2023-09-13 17:20:39 +0200

    fix imports

--- packages/backend/src/persistance/notification/addUsersNotificationChannel.ts
+++ packages/backend/src/persistance/notification/addUsersNotificationChannel.ts
@@ -4,7 +4,7 @@ import {
     schema,
 } from 'dm3-lib-delivery';
 import { Redis, RedisPrefix } from '../getDatabase';
-import { validateSchema } from 'dm3-lib-shared/dist.backend';
+import { validateSchema } from 'dm3-lib-shared';
 import { getIdEnsName } from '../getIdEnsName';
 import { stringify } from 'dm3-lib-shared';
 
