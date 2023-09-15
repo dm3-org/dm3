d7d92bd2940fec5f371a1812f3d0f2343da66a80
AlexNi245 <alex.plutta@googlemail.com>
2023-09-15 11:52:52 +0200

    persist message into chunk

--- packages/lib/storage/src/new/MessageStorage.test.ts
+++ packages/lib/storage/src/new/MessageStorage.test.ts
@@ -11,13 +11,66 @@ describe('MessageStorage', () => {
             encrypt: (val: any) => val,
             decrypt: (val: any) => val,
         };
-        const storage = await MessageStorage(db, enc, rootKey);
-
+        const storage = await MessageStorage(
+            {
+                getNode: db.getNode,
+                addNode: db.addNode,
+            },
+            enc,
+            rootKey,
+        );
         expect(db.nodes.size).toBe(1);
         expect(db.nodes.get(rootKey)).toEqual(JSON.stringify([]));
         expect(storage.getConversations()).toEqual([]);
     });
+    it('first message, creates new conversation, and chunk', async () => {
+        const keyB = '+DpeBjCzICFoi743/466yJunsHR55Bhr3GnqcS4cuJU=';
 
+        const db = testDb();
+        const rootKey = sha256('alice.eth');
+
+        const enc = {
+            //encrypt: (val: any) => val,
+            encrypt: (val: any) => encrypt(keyB, val),
+            //decrypt: (val: any) => val,
+            decrypt: (val: any) => decrypt(keyB, val),
+        };
+        const storage = await MessageStorage(
+            {
+                getNode: db.getNode,
+                addNode: db.addNode,
+            },
+            enc,
+            rootKey,
+        );
+        await storage.addMessage(
+            makeEnvelop('alice.eth', 'bob.eth', 'hello', 123),
+        );
+        const conversations = storage.getConversations();
+        expect(conversations).toEqual(['bob.eth']);
+
+        const conversationNode = storage._tree.getLeafs<any>()[0];
+        expect(conversationNode.key).toBe(sha256(rootKey + 'bob.eth'));
+        expect(storage._tree.key).toBe(rootKey);
+
+        expect(conversationNode.getLeafs().length).toBe(1);
+        const chunkNode = conversationNode.getLeafs()[0];
+
+        expect(chunkNode.key).toBe(
+            sha256(
+                conversationNode.key +
+                    JSON.stringify({
+                        id: 0,
+                        timestamp: 123,
+                    }),
+            ),
+        );
+        expect(chunkNode.getLeafs().length).toBe(1);
+        const envelop = chunkNode.getLeafs()[0];
+        expect(envelop).toEqual(
+            makeEnvelop('alice.eth', 'bob.eth', 'hello', 123),
+        );
+    });
     it('gets conversations', async () => {
         const db = testDb();
         const rootKey = sha256('alice.eth');
@@ -26,25 +79,18 @@ describe('MessageStorage', () => {
             encrypt: (val: any) => val,
             decrypt: (val: any) => val,
         };
-        const storage = await MessageStorage(db, enc, rootKey);
+        const storage = await MessageStorage(
+            {
+                getNode: db.getNode,
+                addNode: db.addNode,
+            },
+            enc,
+            rootKey,
+        );
 
-        await storage.addMessage(makeEnvelop('alice.eth', 'bob.eth', 'hello'));
-        const conversations = storage.getConversations();
-        expect(conversations).toEqual(['bob.eth']);
-    });
-    it('encryptes and decryptes conversations', async () => {
-        const keyB = '+DpeBjCzICFoi743/466yJunsHR55Bhr3GnqcS4cuJU=';
-
-        const db = testDb();
-        const rootKey = sha256('alice.eth');
-
-        const enc = {
-            encrypt: (val: any) => encrypt(keyB, val),
-            decrypt: (val: any) => decrypt(keyB, val),
-        };
-        const storage = await MessageStorage(db, enc, rootKey);
-
-        await storage.addMessage(makeEnvelop('alice.eth', 'bob.eth', 'hello'));
+        await storage.addMessage(
+            makeEnvelop('alice.eth', 'bob.eth', 'hello', 123),
+        );
         const conversations = storage.getConversations();
         expect(conversations).toEqual(['bob.eth']);
     });
--- packages/lib/storage/src/new/MessageStorage.ts
+++ packages/lib/storage/src/new/MessageStorage.ts
@@ -22,36 +22,53 @@ export const MessageStorage = async (
     const addMessage = (envelop: Envelop) => root.add(envelop);
     const getConversations = () => root.conversationNames;
 
-    return { addMessage, getConversations };
+    return { addMessage, getConversations, _tree: root };
 };
 
 type Leaf = Node | Envelop;
 
+type ChunkIdentifier = {
+    id: number;
+    timestamp: number;
+};
+
+const DEFAULT_SIZE_LIMIT = 1000;
 abstract class Node {
     public readonly key: string;
     public readonly db: IStorageDatabase;
     public readonly enc: IStorageEncryption;
 
-    private readonly leafs: Leaf[];
-
-    public getLeafs<T extends Leaf>() {
-        return this.leafs as T[];
-    }
-    public addLeaf(leaf: Leaf) {
-        this.leafs.push(leaf);
-    }
+    private readonly sizeLimit: number;
+    private leafs: Leaf[];
 
     protected constructor(
         db: IStorageDatabase,
         enc: IStorageEncryption,
         key: string,
         children: Node[] | Envelop[],
+        sizeLimit = DEFAULT_SIZE_LIMIT,
     ) {
         this.key = key;
         this.db = db;
         this.enc = enc;
         this.leafs = children;
+        this.sizeLimit = sizeLimit;
     }
+    public getLeafs<T extends Leaf>() {
+        return this.leafs as T[];
+    }
+    public addLeaf(leaf: Leaf) {
+        this.leafs.push(leaf);
+    }
+    public setLeafs(leafs: Leaf[]) {
+        this.leafs = leafs;
+    }
+
+    public hasSpace(newLeaf: Leaf) {
+        const newLeafs = JSON.stringify([...this.leafs, newLeaf]);
+        return Buffer.byteLength(newLeafs, 'utf-8') < this.sizeLimit;
+    }
+
     protected async save() {
         const encrypted = await this.enc.encrypt(this.serialize());
         await this.db.addNode(this.key, encrypted);
@@ -131,7 +148,7 @@ class Root extends Node {
             //Add the conversation name to the conversation list
             this.conversationNames.push(conversationName);
         }
-        // return (conversation as Node).add(envelop);
+        return await conversation.add(envelop);
     }
 }
 
@@ -172,15 +189,109 @@ class Conversation extends Node {
     }
 
     public async add(message: Envelop) {
-        //If message fits into the existing chunk add it
-        this.getLeafs<Node>()[this.getLeafs<Node>().length - 1].add(message);
-        //Else add new chunk
-        //And add it there
+        //Fetch conversatin chunks to get the last lists of chunks
+        const serializedChunks = await this.db.getNode(this.key);
+        if (!serializedChunks) {
+            //Shold not happen in because a conversation is always created before
+            throw 'Conversation does not exist yet';
+        }
+        // deserialize the encrypted chunks
+        const chunkIdentifier = await Chunk.deserialize(
+            this.enc,
+            serializedChunks,
+        );
+        //map the chunk identifier to chunk instances
+        const chunkInstances = chunkIdentifier.map(
+            (chunk: any) => new Chunk(this.db, this.enc, chunk.id, []),
+        );
+        //update the conversation with the latest list of chunks
+        this.setLeafs(chunkInstances);
+
+        // the first chunk has to be created
+        if (this.getLeafs<Node>().length === 0) {
+            //Create the first chunk
+            const emptyChunIdentifer: ChunkIdentifier = {
+                //0 is the first index
+                id: 0,
+                //the timestamp of the first message
+                timestamp: message.message.metadata.timestamp,
+            };
+            const chunk = await Chunk.createAndSafe(
+                this.db,
+                this.enc,
+                Chunk.computeKey(this.key, emptyChunIdentifer),
+                [],
+            );
+            //Add the chunk to the conversation
+            this.addLeaf(chunk);
+        }
+
+        //get the latest chunk
+        const latestChunk = this.getLeafs<Chunk>()[this.getLeafs().length - 1];
+        //Fetch all envelops of the chunk to determine if the chunk is full
+        await latestChunk.fetch();
+
+        if (latestChunk.hasSpace(message)) {
+            //Add the message to the chunk
+            await latestChunk.add(message);
+            return;
+        }
+
+        //If the chunk is full create a new chunk
+        const emptyChunkIdentifer: ChunkIdentifier = {
+            //the id is the number of chunks
+            id: this.getLeafs().length,
+            //the timestamp of the first message
+            timestamp: message.message.metadata.timestamp,
+        };
+        //Create new chunk
+        const emptyChunk = await Chunk.createAndSafe(
+            this.db,
+            this.enc,
+            Chunk.computeKey(this.key, emptyChunkIdentifer),
+            [],
+        );
+        //Add the chunk to the conversation
+        this.addLeaf(emptyChunk);
+        //finally the chunk that'll contain the message has been created and sits at the end of the list
+        await emptyChunk.add(message);
     }
 }
 
 class Chunk extends Node {
+    public static async createAndSafe(
+        db: IStorageDatabase,
+        enc: IStorageEncryption,
+        key: string,
+        children: Node[] | Envelop[],
+    ) {
+        const instance = new Chunk(db, enc, key, children);
+        await instance.save();
+        return instance;
+    }
     public async add(message: Envelop) {
+        //add message to envelop array
         this.addLeaf(message);
+        //persist the chunk
+        await this.save();
+    }
+
+    public async fetch() {
+        const serializedEnvelops = await this.db.getNode(this.key);
+        if (!serializedEnvelops) {
+            //Shold not happen in because the chunk is always created before
+            throw 'Chunk does not exist yet';
+        }
+        // deserialize the encrypted chunks
+        const envelops = await Node.deserialize(this.enc, serializedEnvelops);
+
+        //update the conversation with the latest list of chunks
+        this.setLeafs(envelops);
+    }
+    public static computeKey(
+        conversationKey: string,
+        chunkIdentifier: ChunkIdentifier,
+    ) {
+        return sha256(conversationKey + JSON.stringify(chunkIdentifier));
     }
 }
