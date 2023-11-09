693c917e8f63372bb1e0d7eeefe54ad9c29bc6c4
Heiko Burkhardt <mail@heiko-burkhardt.de>
2023-11-09 12:04:31 +0100

    fix: ws

--- packages/billboard-client/src/service/viewerService/viewerService.ts
+++ packages/billboard-client/src/service/viewerService/viewerService.ts
@@ -2,6 +2,7 @@ import { Message } from 'dm3-lib-messaging';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
 import { IViewerService } from './IViewerService';
+import { logDebug } from 'dm3-lib-shared';
 
 /**
  * Creates and returns an instance of a viewer service that manages viewer connections and message broadcasting.
@@ -20,6 +21,12 @@ export function ViewerService(httpServer: http.Server): IViewerService {
     const connections: Map<string, Socket> = new Map();
 
     const addConnection = (connection: Socket) => {
+        logDebug({
+            text: '[ViewerService] connect',
+            id: connection.id,
+            connectionsSize: connections.size,
+        });
+
         //When the socket disconnects we wan't them no longer in our viewers List
         connection.on('disconnect', () => {
             removeConnection(connection);
@@ -28,10 +35,19 @@ export function ViewerService(httpServer: http.Server): IViewerService {
     };
 
     const removeConnection = (connection: Socket) => {
+        logDebug({
+            text: '[ViewerService] disconnect',
+            id: connection.id,
+            connectionsSize: connections.size,
+        });
         connections.delete(connection.id);
     };
 
     const getViewerCount = () => {
+        logDebug({
+            text: '[ViewerService] getViewerCount',
+            connectionsSize: connections.size,
+        });
         return connections.size;
     };
 
