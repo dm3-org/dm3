65ef3ca5081ac381cd053e5854c6df1ae10f9ec5
Steffen Kux <steffen@kuxfamily.de>
2023-11-24 13:45:42 +0100

    background adjustment

--- packages/messenger-web/src/Header.tsx
+++ packages/messenger-web/src/Header.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <nav className="navbar fixed-top navbar-light" >
-            <a href="https://dm3.network" target="_blank" style={{ position: 'absolute', top: '20px', left: '25px' }}>
+            <a href="https://dm3.network" target="_blank" style={{ position: 'absolute', top: '26px', left: '21px' }}>
                 <img
                     src="/dm3-logo.png"
                     alt="dm3 Network Logo"
-                    style={{ height: '50px', transition: 'transform 0.5s ease' }} // Stelle die Größe nach Bedarf ein
-                    onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.1)'}
+                    style={{ height: '29px', transition: 'transform 0.5s ease' }} // Stelle die Größe nach Bedarf ein
+                    onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.2)'}
                     onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'}
                 />
             </a>
