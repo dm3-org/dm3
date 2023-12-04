4ccdeb19e4535ff821a965a9990e32ff7a7489f1
Bhupesh-mfsi <mfsi.bhupeshd@gmail.com>
2023-12-04 17:53:53 +0530

    Widget CSS customization

--- packages/messenger-widget/src/utils/theme-utils.ts
+++ packages/messenger-widget/src/utils/theme-utils.ts
@@ -1,6 +1,4 @@
-export const themeOne: any = {
-    baseBackground:
-        'linear-gradient(25deg, #9341FE -437%, #0D0D0D 71%, #2CA6FF 145%)',
+export const defaultTheme: any = {
     backgroundContainer: '#1A1B22',
     normalBtnBorder: '2px solid #544393',
     configBoxBorder: '1px solid white',
@@ -9,8 +7,7 @@ export const themeOne: any = {
     normalBtnInactive: '#2A2B38',
     textPrimary: '#FFFF',
     textSecondary: '#81828D',
-    backgroundActiveContact:
-        'linear-gradient(89.87deg, rgba(84, 67, 147, 0.5) 1.99%, rgba(31, 32, 41, 0.5) 90.93%)',
+    backgroundActiveContact: 'linear-gradient(-259deg, #544393, #1f2029)',
     backgroundConfigBox: '#3A3C50',
     backgroundConfigBoxBorder: '1px solid #666876',
     chatBackground: '#1F2029',
@@ -18,4 +15,44 @@ export const themeOne: any = {
     textError: '#C30F1A',
     errorBackground: '#830B12',
     attachmentBackground: '#202129',
+};
+
+export const getCustomizedTheme = (themeCss: any) => {
+    return {
+        backgroundContainer:
+            themeCss.backgroundColor ?? defaultTheme.backgroundContainer,
+        normalBtnBorder: themeCss.buttonBorderColor
+            ? `2px solid ${themeCss.buttonBorderColor}`
+            : defaultTheme.normalBtnBorder,
+        configBoxBorder: themeCss.configBoxBorderColor
+            ? `1px solid ${themeCss.configBoxBorderColor}`
+            : defaultTheme.configBoxBorder,
+        normalBtn: themeCss.buttonColor ?? defaultTheme.normalBtn,
+        normalBtnHover:
+            themeCss.hoverButtonColor ?? defaultTheme.normalBtnHover,
+        normalBtnInactive:
+            themeCss.inactiveButtonColor ?? defaultTheme.normalBtnInactive,
+        textPrimary: themeCss.primaryTextColor ?? defaultTheme.textPrimary,
+        textSecondary:
+            themeCss.secondaryTextColor ?? defaultTheme.textSecondary,
+        backgroundActiveContact:
+            themeCss.activeContactBackgroundColor ??
+            defaultTheme.backgroundActiveContact,
+        backgroundConfigBox:
+            themeCss.configurationBoxBackgroundColor ??
+            defaultTheme.backgroundConfigBox,
+        backgroundConfigBoxBorder: themeCss.configurationBoxBorderColor
+            ? `1px solid ${themeCss.configurationBoxBorderColor}`
+            : defaultTheme.backgroundConfigBoxBorder,
+        chatBackground:
+            themeCss.chatBakgroundColor ?? defaultTheme.chatBackground,
+        disabledBtnText:
+            themeCss.disabledButtonTextColor ?? defaultTheme.disabledBtnText,
+        textError: themeCss.errorTextColor ?? defaultTheme.textError,
+        errorBackground:
+            themeCss.errorBackgroundColor ?? defaultTheme.errorBackground,
+        attachmentBackground:
+            themeCss.attachmentBackgroundColor ??
+            defaultTheme.attachmentBackground,
+    };
 };
