78b92cc102e59d5982625e891eb113761e3db6bf
AlexNi245 <alex.plutta@googlemail.com>
2024-01-19 10:20:35 +0100

    stash useStorage

--- packages/messenger-widget/src/hooks/storage/useStorge.test.tsx
+++ packages/messenger-widget/src/hooks/storage/useStorge.test.tsx
@@ -1,13 +1,33 @@
-import { SyncProcessState, useStorage } from './useStorage';
+/* eslint-disable no-console */
 describe('storage', () => {
-    it('should pass', () => {
-        expect(true).toBe(true);
+    let windowSpy: any;
+
+    beforeEach(() => {
+        windowSpy = jest.spyOn(window, 'window', 'get');
     });
-    it('should pass', () => {
-        expect(true).toBe(false);
+
+    afterEach(() => {
+        windowSpy.mockRestore();
     });
+
     describe('initialize', () => {
         it('storage cant be initialized without deliveryServiceToken', () => {
+            Object.defineProperty(global, 'window', {
+                //@ts-ignore
+                sodium: {
+                    onload: function (sodium: any) {
+                        let h = sodium.crypto_generichash(
+                            64,
+                            sodium.from_string('test'),
+                        );
+                        console.log(sodium.to_hex(h));
+                    },
+                },
+            });
+
+            const useStorage = require('./useStorage').useStorage;
+            console.log('check');
+            const { SyncProcessState } = require('../../utils/enum-type-utils');
             const hook = useStorage(undefined, undefined, undefined);
 
             expect(hook).toBeDefined();
