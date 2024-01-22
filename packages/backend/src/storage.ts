f545092ede70127b7d49d0abea6147459707a117
AlexNi245 <alex.plutta@googlemail.com>
2024-01-22 11:32:32 +0100

    use proper commit

--- packages/backend/src/storage.ts
+++ packages/backend/src/storage.ts
@@ -1,9 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import stringify from 'safe-stable-stringify';
-import { WithLocals } from './types';
 import { auth } from './utils';
-import { normalizeEnsName } from 'dm3-lib-profile';
+import { normalizeEnsName } from '@dm3-org/dm3-lib-profile';
 
 export default () => {
     const router = express.Router();
@@ -12,22 +11,26 @@ export default () => {
     router.use(cors());
     router.param('ensName', auth);
 
-    router.get('/:ensName', async (req, res, next) => {
+    router.get('/:ensName/:key', async (req, res, next) => {
         try {
-            const account = normalizeEnsName(req.params.ensName);
-            const userStorage = await req.app.locals.db.getUserStorage(account);
+            const ensName = normalizeEnsName(req.params.ensName);
+            const userStorage = await req.app.locals.db.getUserStorageChunk(
+                ensName,
+                req.params.key,
+            );
             return res.json(userStorage);
         } catch (e) {
             next(e);
         }
     });
 
-    router.post('/:ensName', async (req, res, next) => {
+    router.post('/:ensName/:key', async (req, res, next) => {
         try {
-            const account = normalizeEnsName(req.params.ensName);
+            const ensName = normalizeEnsName(req.params.ensName);
 
-            await req.app.locals.db.setUserStorage(
-                account,
+            await req.app.locals.db.setUserStorageChunk(
+                ensName,
+                req.params.key,
                 stringify(req.body)!,
             );
 
