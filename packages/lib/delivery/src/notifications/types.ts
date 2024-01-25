c7cb6aa0aacda206834581dc3a7e2ff44c6ed154
Bhupesh-mfsi <mfsi.bhupeshd@gmail.com>
2024-01-25 14:37:29 +0530

    modified comments

--- packages/lib/delivery/src/notifications/types.ts
+++ packages/lib/delivery/src/notifications/types.ts
@@ -1,10 +1,19 @@
 import { DeliveryInformation } from '@dm3-org/dm3-lib-messaging';
 
-//Every notification channel that is supported by the delivery service must be listed here
+/**
+ * Every notification channel that is supported by the delivery service must be listed here
+ * Notification channel is medium of getting notified. It can be email, phone no. push notification etc...
+ */
 export enum NotificationChannelType {
     EMAIL = 'EMAIL',
 }
 
+/**
+ * Notification type describes which kind of notification it is :
+ * 1. New Message
+ * 2. Verification OTP
+ * etc...
+ */
 export enum NotificationType {
     NEW_MESSAGE = 'NEW_MESSAGE',
     OTP = 'OTP',
