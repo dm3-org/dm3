547849c786a6b7be2ffeaf1c016a1b52eaae6fb3
Steffen Kux <steffen@kuxfamily.de>
2024-03-20 19:43:39 +0100

    correct properties for env

--- packages/messenger-web/src/Dm3Widget.tsx
+++ packages/messenger-web/src/Dm3Widget.tsx
@@ -12,9 +12,19 @@ const Dm3Widget: React.FC = () => {
     // Define the props for the DM3 component
     const props: any = {
         defaultContact: 'contact.dm3.eth',
-        defaultServiceUrl: process.env.REACT_APP_DEFAULT_SERVICE,
-        ethereumProvider: process.env.REACT_APP_MAINNET_PROVIDER_RPC,
-        walletConnectProjectId: process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID,
+
+        userEnsSubdomain: process.env.REACT_APP_USER_ENS_SUBDOMAIN as string,
+        addressEnsSubdomain: process.env.REACT_APP_ADDR_ENS_SUBDOMAIN as string,
+        resolverBackendUrl: process.env.REACT_APP_RESOLVER_BACKEND as string,
+        profileBaseUrl: process.env.REACT_APP_PROFILE_BASE_URL as string,
+        defaultDeliveryService: process.env.REACT_APP_DEFAULT_DELIVERY_SERVICE as string,
+        backendUrl: process.env.REACT_APP_BACKEND as string,
+        chainId: process.env.REACT_APP_CHAIN_ID as string,
+        resolverAddress: process.env.REACT_APP_RESOLVER_ADDR as string,
+        defaultServiceUrl: process.env.REACT_APP_DEFAULT_SERVICE as string,
+        ethereumProvider: process.env.REACT_APP_ETHEREUM_PROVIDER as string,
+        walletConnectProjectId: process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID as string,
+
         showAlways: true,
         hideFunction: 'attachments', // Optional parameter: 'attachments,edit,delete' or undefined
         showContacts: true, // true for all contacts / false for default contact
