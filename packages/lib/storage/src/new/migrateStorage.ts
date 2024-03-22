4d1a202d4788559991fbd3f85c64bedffdb4315b
AlexNi245 <alex.plutta@googlemail.com>
2024-03-22 10:27:45 +0100

    add logs

--- packages/lib/storage/src/new/migrateStorage.ts
+++ packages/lib/storage/src/new/migrateStorage.ts
@@ -6,12 +6,14 @@ export const migrageStorage = async (
     newStorage: StorageAPI,
     resolveTLDtoAlias: (tldDomain: string) => Promise<string>,
 ) => {
+    console.log('start storage migration');
     const conversations = oldStorage.conversations;
     //get keys of conversations map
     const keys = Array.from(conversations.keys());
 
     await Promise.all(
         keys.map(async (contactName) => {
+            console.log('migrating ', contactName);
             const aliasName = await resolveTLDtoAlias(contactName);
             const messages = await oldStorage.conversations.get(contactName);
             await newStorage.addMessageBatch(aliasName, messages ?? []);
