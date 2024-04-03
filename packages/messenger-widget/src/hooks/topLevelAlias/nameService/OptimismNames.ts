cf6d6deae6253caf121f3d2e13ca0197d5a4dc4e
AlexNi245 <alex.plutta@googlemail.com>
2024-04-03 17:02:30 +0200

    add Optimismnames TLD resolver

---
+++ packages/messenger-widget/src/hooks/topLevelAlias/nameService/OptimismNames.ts
@@ -0,0 +1,181 @@
+import { ethers } from 'ethers';
+import { ITLDResolver } from './TLDResolver';
+import { globalConfig } from '@dm3-org/dm3-lib-shared';
+
+const TOP_LEVEL_DOMAIN = '.op.dm3.eth';
+const EVM_FETCHER_CONTRACT_ADDRESS =
+    '0x2BAD1FeC0a2629757470984284C11EA00adB8E6F';
+
+function getIdForAddress(address: string) {
+    return address + globalConfig.ADDR_ENS_SUBDOMAIN();
+}
+
+export class OptimismNames implements ITLDResolver {
+    private readonly provider: ethers.providers.JsonRpcProvider;
+
+    constructor(provider: ethers.providers.JsonRpcProvider) {
+        this.provider = provider;
+    }
+    //e.g. max.op.dm3.eth => 0x1234.addr.dm3.eth
+    async isResolverForTldName(ensName: string): Promise<boolean> {
+        const isGnoDomain = ensName.endsWith(TOP_LEVEL_DOMAIN);
+
+        if (!isGnoDomain) {
+            return false;
+        }
+        return this.hasDm3ProfileOnEnsProfile(ensName);
+    }
+    //e.g. 0x1234.addr.dm3.eth => max.op.dm3.eth
+    async isResolverForAliasName(ensName: string): Promise<boolean> {
+        const address = ensName.split('.')[0];
+        if (!ethers.utils.isAddress(address)) {
+            return false;
+        }
+        //Use the reverse record to get the owner of the name
+        const reverseNode = `${address.slice(2).toLowerCase()}.addr.reverse`;
+        const name = await this.lookupAddressCcip(ensName, reverseNode);
+
+        return this.hasDm3ProfileOnEnsProfile(name);
+    }
+    //e.g. 0x1234.addr.dm3.eth => max.eth
+    async resolveAliasToTLD(ensName: string): Promise<string> {
+        const address = ensName.split('.')[0];
+        if (!ethers.utils.isAddress(address)) {
+            return ensName;
+        }
+        //Use the reverse record to get the owner of the name
+        const reverseNode = `${address.slice(2).toLowerCase()}.addr.reverse`;
+        const resolvedName = await this.lookupAddressCcip(ensName, reverseNode);
+        return resolvedName ?? ensName;
+    }
+    //e.g. max.op.dm3eth => 0x1234.addr.dm3.eth
+    async resolveTLDtoAlias(ensName: string): Promise<string> {
+        const address = await this.resolveNameCcip(ensName);
+        if (!address) {
+            throw new Error('No address found for ' + ensName);
+        }
+        return getIdForAddress(ethers.utils.getAddress(address));
+    }
+    private async hasDm3ProfileOnEnsProfile(opName: string): Promise<boolean> {
+        try {
+            const aliasName = opName;
+            const ensNameHasAddress = await this.resolveNameCcip(aliasName);
+
+            const dm3Profile = await this.resolveTextCcip(
+                aliasName,
+                'network.dm3.profile',
+            );
+
+            if (!dm3Profile) {
+                return false;
+            }
+
+            return !!ensNameHasAddress;
+        } catch (err) {
+            console.log(
+                `Cant resolve OP name ${opName} to address error: ${err}`,
+            );
+            return false;
+        }
+    }
+    private getEvmFetcher() {
+        return new ethers.utils.Interface([
+            'function addr(bytes32) returns(address)',
+            'function name(bytes32) returns(string)',
+            'function text(bytes32 node, string calldata key) external view returns (string memory)',
+            'function resolve(bytes,bytes) returns (bytes memory result)',
+        ]);
+    }
+    //Helper function to retrive the address of a given name from the Dm3NameRegistrar contract using the EVM FETCHER
+    private async resolveNameCcip(opName: string) {
+        try {
+            const i = this.getEvmFetcher();
+            const node = ethers.utils.namehash(opName);
+            const innerReq = i.encodeFunctionData('addr', [node]);
+            const outerReq = i.encodeFunctionData('resolve', [
+                ethers.utils.dnsEncode(opName),
+                innerReq,
+            ]);
+
+            const res = await this.provider.call({
+                to: EVM_FETCHER_CONTRACT_ADDRESS,
+                data: outerReq,
+                ccipReadEnabled: true,
+            });
+
+            const decodedOuter = i.decodeFunctionResult('resolve', res);
+            const decodedInner = i.decodeFunctionResult(
+                'addr',
+                decodedOuter[0],
+            );
+
+            const address = decodedInner[0];
+
+            if (address === ethers.constants.AddressZero) {
+                return null;
+            }
+            return address;
+        } catch (err) {
+            console.log('Error resolving name op name ', err);
+            return null;
+        }
+    }
+    //Helper function to retrive the name of a given address from the Dm3NameRegistrar contract using the EVM FETCHER
+    //
+    private async lookupAddressCcip(opName: string, reverseNode: string) {
+        try {
+            const i = this.getEvmFetcher();
+            const innerReq = i.encodeFunctionData('name', [reverseNode]);
+            const outerReq = i.encodeFunctionData('resolve', [
+                ethers.utils.dnsEncode(opName),
+                innerReq,
+            ]);
+
+            const res = await this.provider.call({
+                to: EVM_FETCHER_CONTRACT_ADDRESS,
+                data: outerReq,
+                ccipReadEnabled: true,
+            });
+
+            const decodedOuter = i.decodeFunctionResult('resolve', res);
+            const decodedInner = i.decodeFunctionResult(
+                'name',
+                decodedOuter[0],
+            );
+            return decodedInner[0];
+        } catch (err) {
+            console.log('Error resolving name op name ', err);
+            return null;
+        }
+    }
+    //Helper function to retrive a text record of a given name from the Dm3NameRegistrar contract using the EVM FETCHER
+
+    private async resolveTextCcip(opName: string, key: string) {
+        try {
+            const i = this.getEvmFetcher();
+            const node = ethers.utils.namehash(opName);
+            const innerReq = i.encodeFunctionData('text', [node, key]);
+            const outerReq = i.encodeFunctionData('resolve', [
+                ethers.utils.dnsEncode(opName),
+                innerReq,
+            ]);
+
+            const res = await this.provider.call({
+                to: EVM_FETCHER_CONTRACT_ADDRESS,
+                data: outerReq,
+                ccipReadEnabled: true,
+            });
+
+            const decodedOuter = i.decodeFunctionResult('resolve', res);
+            const decodedInner = i.decodeFunctionResult(
+                'text',
+                decodedOuter[0],
+            );
+
+            return decodedInner[0];
+        } catch (err) {
+            console.log('Error resolving text for op name', err);
+            return null;
+        }
+    }
+}
