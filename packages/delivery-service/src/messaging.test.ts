9f2c6c15a795196c1d49a82ef9e159c214d8e7a6
malteish <malteish@corpus.io>
2024-04-19 11:47:40 +0200

    fix messaging tests

--- packages/delivery-service/src/messaging.test.ts
+++ packages/delivery-service/src/messaging.test.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import { Socket } from 'socket.io';
 import { onConnection } from './messaging';
 import { testData } from '../../../test-data/encrypted-envelops.test';
-import { WithLocals } from './types';
 import { Session } from '@dm3-org/dm3-lib-delivery';
 import { UserProfile } from '@dm3-org/dm3-lib-profile';
 import { createKeyPair } from '@dm3-org/dm3-lib-crypto';
 import { ethersHelper } from '@dm3-org/dm3-lib-shared';
 import winston from 'winston';
+import { getWeb3Provider } from '../../lib/server-side/dist/utils';
 const SENDER_ADDRESS = '0x25A643B6e52864d0eD816F1E43c0CF49C83B8292';
 const RECEIVER_ADDRESS = '0xDd36ae7F9a8E34FACf1e110c6e9d37D0dc917855';
 
@@ -29,308 +29,6 @@ const keysA = {
 };
 
 const keyPair = createKeyPair();
-
-describe('Messaging', () => {
-    describe('submitMessage', () => {
-        it('returns success if schema is valid', (done: any) => {
-            //We expect the callback functions called once witht he value 'success'
-            expect.assertions(1);
-            const callback = (e: any) => {
-                // eslint-disable-next-line max-len
-                //Even though the method fails jest dosen't recognize it becuase of the catch block used in messaging.ts. So we have to throw another error if the callback returns anything else then the expected result.
-                if (e.response !== 'success') {
-                    throw Error(e);
-                }
-                expect(e.response).toBe('success');
-                done();
-            };
-            //We provide an mocked express app with all needes locals vars
-            const app = {
-                locals: {
-                    logger,
-                    keys: {
-                        signing: keysA.signingKeyPair,
-                        encryption: keysA.encryptionKeyPair,
-                    },
-
-                    deliveryServiceProperties: {
-                        sizeLimit: 2 ** 14,
-                        notificationChannel: [],
-                    },
-                    web3Provider: {
-                        resolveName: async () =>
-                            '0x25A643B6e52864d0eD816F1E43c0CF49C83B8292',
-                    },
-
-                    db: {
-                        getSession,
-                        createMessage: () => {},
-                        getIdEnsName: async (ensName: string) => ensName,
-                        getUsersNotificationChannels: () => Promise.resolve([]),
-                    },
-                    io: {
-                        sockets: {
-                            to: (_: any) => ({
-                                emit: (_: any, __any: any) => {},
-                            }),
-                        },
-                    },
-                } as any,
-            } as express.Express & WithLocals;
-
-            //The same data used in Messages.test
-            const data = {
-                envelop: testData.envelopA,
-                token: '123',
-            };
-
-            const getSocketMock = () => {
-                return {
-                    on: async (name: string, onSubmitMessage: any) => {
-                        //We just want to test the submitMessage callback fn
-                        if (name === 'submitMessage') {
-                            await onSubmitMessage(data, callback);
-                        }
-                    },
-                } as unknown as Socket;
-            };
-
-            onConnection(app)(getSocketMock());
-        });
-        it('sends notification after message was submitted', (done: any) => {
-            //We expect the callback functions called once witht he value 'success'
-            expect.assertions(1);
-            const callback = (e: any) => {
-                // eslint-disable-next-line max-len
-                //Even though the method fails jest dosen't recognize it becuase of the catch block used in messaging.ts. So we have to throw another error if the callback returns anything else then the expected result.
-                if (e.response !== 'success') {
-                    throw Error(e);
-                }
-                expect(e.response).toBe('success');
-                done();
-            };
-            //We provide an mocked express app with all needes locals vars
-            const app = {
-                locals: {
-                    logger,
-                    keys: {
-                        signing: keysA.signingKeyPair,
-                        encryption: keysA.encryptionKeyPair,
-                    },
-
-                    deliveryServiceProperties: {
-                        sizeLimit: 2 ** 14,
-                        notificationChannel: [],
-                    },
-                    web3Provider: {
-                        resolveName: async () =>
-                            '0x25A643B6e52864d0eD816F1E43c0CF49C83B8292',
-                    },
-
-                    db: {
-                        getSession,
-                        createMessage: () => {},
-                        getIdEnsName: async (ensName: string) => ensName,
-                        getUsersNotificationChannels: () => Promise.resolve([]),
-                    },
-                    io: {
-                        sockets: {
-                            to: (_: any) => ({
-                                emit: (_: any, __any: any) => {},
-                            }),
-                        },
-                    },
-                } as any,
-            } as express.Express & WithLocals;
-
-            //The same data used in Messages.test
-            const data = {
-                envelop: testData.envelopA,
-                token: '123',
-            };
-
-            const getSocketMock = () => {
-                return {
-                    on: async (name: string, onSubmitMessage: any) => {
-                        //We just want to test the submitMessage callback fn
-                        if (name === 'submitMessage') {
-                            await onSubmitMessage(data, callback);
-                        }
-                    },
-                } as unknown as Socket;
-            };
-
-            onConnection(app)(getSocketMock());
-        });
-        it.skip('returns error if message is spam', (done: any) => {
-            //We expect the callback functions called once witht he value 'success'
-            expect.assertions(1);
-            const callback = jest.fn((e: any) => {
-                // eslint-disable-next-line max-len
-                //Even though the method fails jest dosen't recognize it becuase of the catch block used in messaging.ts. So we have to throw another error if the callback returns anything else then the expected result.
-                if (e.error !== 'Message does not match spam criteria') {
-                    throw Error(e);
-                }
-                expect(e.error).toBe('Message does not match spam criteria');
-                done();
-            });
-            const session = async (addr: string) => {
-                return {
-                    ...(await getSession(addr)),
-                    spamFilterRules: { minNonce: 2 },
-                } as Session;
-            };
-            //We provide an mocked express app with all needes locals vars
-
-            const app = {
-                locals: {
-                    logger,
-                    keys: {
-                        signing: keysA.signingKeyPair,
-                        encryption: keysA.encryptionKeyPair,
-                    },
-
-                    deliveryServiceProperties: {
-                        sizeLimit: 2 ** 14,
-                        notificationChannel: [],
-                    },
-
-                    db: {
-                        getSession: session,
-                        createMessage: () => {},
-                        getIdEnsName: async (ensName: string) => ensName,
-                        getUsersNotificationChannels: () => Promise.resolve([]),
-                    },
-                    web3Provider: {
-                        getTransactionCount: (_: string) => Promise.resolve(0),
-                        resolveName: async () =>
-                            '0x25A643B6e52864d0eD816F1E43c0CF49C83B8292',
-                    },
-                    redisClient: {
-                        zAdd: () => {},
-                    },
-                    io: {
-                        sockets: {
-                            to: (_: any) => ({
-                                emit: (_: any, __any: any) => {},
-                            }),
-                        },
-                    },
-                } as any,
-            } as express.Express & WithLocals;
-
-            //The same data used in Messages.test
-            const data = {
-                envelop: testData.envelopA,
-                token: '123',
-            };
-
-            const getSocketMock = () => {
-                return {
-                    on: async (name: string, onSubmitMessage: any) => {
-                        //We just want to test the submitMessage callback fn
-                        if (name === 'submitMessage') {
-                            await onSubmitMessage(data, callback);
-                        }
-                    },
-                } as unknown as Socket;
-            };
-
-            onConnection(app)(getSocketMock());
-        });
-        it('Throws error if schema is invalid', async () => {
-            //We expect the callback functions called once witht he value 'success'
-            expect.assertions(1);
-            const callback = jest.fn((e: any) => {
-                // eslint-disable-next-line max-len
-                //Even though the method fails jest dosen't recognize it becuase of the catch block used in messaging.ts. So we have to throw another error if the callback returns anything else then the expected result.
-                if (e.error !== 'invalid schema') {
-                    throw Error(e);
-                }
-                expect(e.error).toBe('invalid schema');
-            });
-            //We provide an mocked express app with all needes locals vars
-            const app = {
-                locals: {
-                    logger,
-                    deliveryServicePrivateKey:
-                        '9SZhajjn9tn0fX/eBMXfZfb0RaUeYyfhlNYHqZyKHpyTiYvwVosQ5qt2XxdDFblTzggir8kp85kWw76p2EZ0rQ==',
-                    redisClient: {
-                        zAdd: () => {},
-                    },
-                    io: {
-                        sockets: {
-                            to: (_: any) => ({
-                                emit: (_: any, __any: any) => {},
-                            }),
-                        },
-                    },
-                    db: {
-                        getSession,
-                        getIdEnsName: async (ensName: string) => ensName,
-                        getUsersNotificationChannels: () => Promise.resolve([]),
-                    },
-                } as any,
-            } as express.Express & WithLocals;
-
-            //The same data used in Messages.test
-            const data = {
-                envelop: {
-                    foo: 'bar',
-                },
-                token: '123',
-            };
-
-            const getSocketMock = jest.fn(() => {
-                return {
-                    on: async (name: string, onSubmitMessage: any) => {
-                        //We just want to test the submitMessage callback fn
-                        if (name === 'submitMessage') {
-                            await onSubmitMessage(data, callback);
-                        }
-                    },
-                } as unknown as Socket;
-            });
-
-            await onConnection(app)(getSocketMock());
-        });
-    });
-
-    describe('pendingMessage', () => {
-        it('returns error if schema is invalid', async () => {
-            const app = {
-                locals: {
-                    logger,
-                } as any,
-            } as express.Express & WithLocals;
-
-            const data = {
-                accountAddress: '',
-                contactAddress: '',
-            };
-
-            const callback = jest.fn((e: any) => {
-                if (e.error !== 'invalid schema') {
-                    throw Error(e);
-                }
-                expect(e.error).toBe('invalid schema');
-            });
-
-            const getSocketMock = jest.fn(() => {
-                return {
-                    on: async (name: string, onPendingMessage: any) => {
-                        //We just want to test the submitMessage callback fn
-                        if (name === 'pendingMessage') {
-                            await onPendingMessage(data, callback);
-                        }
-                    },
-                } as unknown as Socket;
-            });
-
-            await onConnection(app)(getSocketMock());
-        });
-    });
-});
 
 const getSession = async (address: string) => {
     const emptyProfile: UserProfile = {
@@ -363,3 +61,173 @@ const getSession = async (address: string) => {
 
     return null;
 };
+
+describe('Messaging', () => {
+    // prepare some mocks that are used by many tests
+    const web3Provider = {
+        resolveName: async () => '0x25A643B6e52864d0eD816F1E43c0CF49C83B8292',
+    };
+    const db = {
+        getSession,
+        createMessage: () => {},
+        getIdEnsName: async (ensName: string) => ensName,
+        getUsersNotificationChannels: () => Promise.resolve([]),
+    };
+    const io = {
+        sockets: {
+            to: (_: any) => ({
+                emit: (_: any, __any: any) => {},
+            }),
+        },
+    };
+    //The same data used in Messages.test
+    const data = {
+        envelop: testData.envelopA,
+        token: '123',
+    };
+
+    describe('submitMessage', () => {
+        it('returns success if schema is valid', (done: any) => {
+            //We expect the callback function to be called once with
+            // the value 'success'
+            expect.assertions(1);
+
+            const callback = (e: any) => {
+                // Even though the method fails jest doesn't recognize it because
+                // of the catch block used in messaging.ts. So we have to throw
+                // another error if the callback returns anything else then the expected
+                // result.
+                expect(e.response).toBe('success');
+                done();
+            };
+
+            const getSocketMock = () => {
+                return {
+                    on: async (name: string, onSubmitMessage: any) => {
+                        //We just want to test the submitMessage callback fn
+                        if (name === 'submitMessage') {
+                            await onSubmitMessage(data, callback);
+                        }
+                    },
+                } as unknown as Socket;
+            };
+
+            onConnection(
+                io as any,
+                web3Provider as any,
+                db as any,
+                keysA,
+            )(getSocketMock());
+        });
+        it.skip('returns error if message is spam', (done: any) => {
+            //We expect the callback functions called once with the value 'success'
+            expect.assertions(1);
+            const callback = jest.fn((e: any) => {
+                /*
+                 * Even though the method fails jest dosen't recognize it because of the catch block
+                 * used in messaging.ts. So we have to throw another error if the callback returns
+                 * anything else then the expected result.
+                 */
+                expect(e.error).toBe('Message does not match spam criteria');
+                done();
+            });
+
+            const session = async (addr: string) => {
+                return {
+                    ...(await getSession(addr)),
+                    spamFilterRules: { minNonce: 2 },
+                } as Session;
+            };
+            const db = {
+                getSession: session,
+                createMessage: () => {},
+                getIdEnsName: async (ensName: string) => ensName,
+                getUsersNotificationChannels: () => Promise.resolve([]),
+            };
+
+            const getSocketMock = () => {
+                return {
+                    on: async (name: string, onSubmitMessage: any) => {
+                        //We just want to test the submitMessage callback fn
+                        if (name === 'submitMessage') {
+                            await onSubmitMessage(data, callback);
+                        }
+                    },
+                } as unknown as Socket;
+            };
+
+            onConnection(
+                io as any,
+                web3Provider as any,
+                db as any,
+                keysA,
+            )(getSocketMock());
+        });
+        it('Throws error if schema is invalid', async () => {
+            expect.assertions(1);
+            const callback = jest.fn((e: any) => {
+                /**
+                 * Even though the method fails jest dosen't recognize it because of the
+                 * catch block used in messaging.ts. So we have to throw another error if
+                 * the callback returns anything else then the expected result.
+                 */
+                expect(e.error).toBe('invalid schema');
+            });
+
+            const localData = { ...data };
+            const metadata = localData.envelop.metadata as any;
+            // Change the data so it becomes invalid in order to provoke the 'invalid schema' error
+            delete metadata.deliveryInformation;
+            localData.envelop.metadata = metadata;
+
+            const getSocketMock = jest.fn(() => {
+                return {
+                    on: async (name: string, onSubmitMessage: any) => {
+                        //We just want to test the submitMessage callback fn
+                        if (name === 'submitMessage') {
+                            await onSubmitMessage(localData, await callback);
+                        }
+                    },
+                } as unknown as Socket;
+            });
+
+            onConnection(
+                io as any,
+                web3Provider as any,
+                db as any,
+                keysA,
+            )(getSocketMock());
+        });
+    });
+
+    describe('pendingMessage', () => {
+        it('returns error if schema is invalid', async () => {
+            const data = {
+                accountAddress: '',
+                contactAddress: '',
+            };
+            const callback = jest.fn((e: any) => {
+                if (e.error !== 'invalid schema') {
+                    throw Error(e);
+                }
+                expect(e.error).toBe('invalid schema');
+            });
+            const getSocketMock = jest.fn(() => {
+                return {
+                    on: async (name: string, onPendingMessage: any) => {
+                        //We just want to test the submitMessage callback fn
+                        if (name === 'pendingMessage') {
+                            await onPendingMessage(data, callback);
+                        }
+                    },
+                } as unknown as Socket;
+            });
+            onConnection(
+                io as any,
+                web3Provider as any,
+                db as any,
+                keysA,
+            )(getSocketMock());
+        });
+    });
+});
