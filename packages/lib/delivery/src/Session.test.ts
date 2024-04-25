8e4fc91eedb6f9cc3aee58044f3bdf01b1c261ad
malteish <malteish@corpus.io>
2024-04-25 15:24:15 +0200

    add tests for jwt validation

--- packages/lib/delivery/src/Session.test.ts
+++ packages/lib/delivery/src/Session.test.ts
@@ -1,11 +1,18 @@
 import { checkToken, Session } from './Session';
+import { sign } from 'jsonwebtoken';
+
+const serverSecret = 'veryImportantSecretToGenerateAndValidateJSONWebTokens';
+// create valid jwt
+const token = sign({ user: 'alice.eth' }, serverSecret, {
+    expiresIn: '1h',
+});
 
 describe('Session', () => {
     describe('checkToken', () => {
-        it('Should return true if a session exists for this account', async () => {
+        it('Should return true if the jwt is valid', async () => {
             const getSession = (_: string) =>
                 Promise.resolve({
-                    token: 'foo',
+                    token: token,
                     createdAt: new Date().getTime(),
                 } as Session);
 
@@ -16,7 +23,8 @@ describe('Session', () => {
                 } as any,
                 getSession,
                 'alice.eth',
-                'foo',
+                token,
+                serverSecret,
             );
 
             expect(isValid).toBe(true);
@@ -32,13 +40,17 @@ describe('Session', () => {
                 } as any,
                 getSession,
                 'alice.eth',
-                'foo',
+                token,
+                serverSecret,
             );
 
             expect(isValid).toBe(false);
         });
 
-        it('Should return false if a session exists but the token is wrong ', async () => {
+        it('Should return false if the token is signed with a different secret ', async () => {
+            const token = sign({ user: 'alice.eth' }, 'attackersSecret', {
+                expiresIn: '1h',
+            });
             const getSession = (_: string) =>
                 Promise.resolve({ token: 'bar' } as Session);
 
@@ -49,7 +61,8 @@ describe('Session', () => {
                 } as any,
                 getSession,
                 'alice.eth',
-                'foo',
+                token,
+                serverSecret,
             );
 
             expect(isValid).toBe(false);
@@ -57,7 +70,14 @@ describe('Session', () => {
 
         it('Should return false if a session exists but the token is expired ', async () => {
             const getSession = (_: string) =>
-                Promise.resolve({ token: 'foo', createdAt: 1 } as Session);
+                Promise.resolve({ token: token, createdAt: 1 } as Session);
+
+            const oneMinuteAgo = new Date().getTime() / 1000 - 60;
+            // this token expired a minute ago
+            const _token = sign(
+                { user: 'alice.eth', iat: oneMinuteAgo, exp: oneMinuteAgo },
+                serverSecret,
+            );
 
             const isValid = await checkToken(
                 {
@@ -66,7 +86,37 @@ describe('Session', () => {
                 } as any,
                 getSession,
                 'alice.eth',
-                'foo',
+                _token,
+                serverSecret,
+            );
+
+            expect(isValid).toBe(false);
+        });
+
+        it('Should return false token issuance date is in the future ', async () => {
+            const getSession = (_: string) =>
+                Promise.resolve({ token: token, createdAt: 1 } as Session);
+
+            const oneMinuteInTheFuture = new Date().getTime() / 1000 + 60;
+            // this token is not valid yet
+            const _token = sign(
+                {
+                    user: 'alice.eth',
+                    iat: oneMinuteInTheFuture,
+                    exp: oneMinuteInTheFuture,
+                },
+                serverSecret,
+            );
+
+            const isValid = await checkToken(
+                {
+                    resolveName: async () =>
+                        '0x25A643B6e52864d0eD816F1E43c0CF49C83B8292',
+                } as any,
+                getSession,
+                'alice.eth',
+                _token,
+                serverSecret,
             );
 
             expect(isValid).toBe(false);
