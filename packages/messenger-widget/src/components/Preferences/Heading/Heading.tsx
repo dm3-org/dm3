a22e4aaa3d5d3a4ebcf5256c32b25eb516066ada
Bhupesh-MS <mfsi.bhupeshd@gmail.com>
2024-05-02 19:07:43 +0530

    removed back icon & added close icon in preferences

--- packages/messenger-widget/src/components/Preferences/Heading/Heading.tsx
+++ packages/messenger-widget/src/components/Preferences/Heading/Heading.tsx
@@ -1,4 +1,8 @@
 import './../Preferences.css';
+import { useContext } from 'react';
+import closeIcon from '../../../assets/images/cross.svg';
+import { ModalContext } from '../../../context/ModalContext';
+import { closeConfigurationModal } from '../../ConfigureProfile/bl';
 
 export interface IHeading {
     heading: string;
@@ -6,14 +10,28 @@ export interface IHeading {
 }
 
 export function Heading(props: IHeading) {
+    const { setShowPreferencesModal, setShowProfileConfigurationModal } =
+        useContext(ModalContext);
+
     return (
-        <div className="background-container preferences-content">
-            <h4 className="mb-0 font-weight-800 preferences-topic">
-                {props.heading}
-            </h4>
-            <span className="font-weight-500 font-size-12">
-                {props.description}
-            </span>
+        <div className="background-container d-flex justify-content-between">
+            <div className="preferences-content">
+                <h4 className="mb-0 font-weight-800 preferences-topic">
+                    {props.heading}
+                </h4>
+                <span className="font-weight-500 font-size-12">
+                    {props.description}
+                </span>
+            </div>
+            <img
+                className="close-modal-icon m-2"
+                src={closeIcon}
+                alt="close"
+                onClick={() => {
+                    setShowPreferencesModal(false);
+                    closeConfigurationModal(setShowProfileConfigurationModal);
+                }}
+            />
         </div>
     );
 }
