027295a6f0d308a347fb9abaa6e446306a4a6246
AlexNi245 <alex.plutta@googlemail.com>
2024-05-07 16:25:59 +0200

    add comments

--- packages/lib/shared/src/IWebSocketManager.ts
+++ packages/lib/shared/src/IWebSocketManager.ts
@@ -1,3 +1,6 @@
+//Interface for an object that manages everything related to WebSocket connections in a single place. This
+//can be used to by any component to use the WebSocket connectivity.
+//TODO move evything related to WebSocket connection to this interface
 export interface IWebSocketManager {
     isConnected(ensName: string): Promise<boolean>;
 }
