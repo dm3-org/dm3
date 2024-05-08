da4e35f9df2599101b140db5fdfe9767b14ee53e
Bhupesh-MS <mfsi.bhupeshd@gmail.com>
2024-05-08 11:17:40 +0530

    added tests for TLD & DM3 config hooks

---
+++ packages/messenger-widget/src/hooks/configuration/useDM3Configuration.test.ts
@@ -0,0 +1,70 @@
+import '@testing-library/jest-dom';
+import { DM3Configuration } from '../../widget';
+import { useDm3Configuration } from './useDM3Configuration';
+import { SiweValidityStatus } from '../../utils/enum-type-utils';
+import { renderHook, waitFor, act } from '@testing-library/react';
+
+const config: DM3Configuration = {
+    defaultContact: '',
+    defaultServiceUrl: '',
+    ethereumProvider: '',
+    walletConnectProjectId: '',
+    userEnsSubdomain: '',
+    addressEnsSubdomain: '',
+    resolverBackendUrl: '',
+    profileBaseUrl: '',
+    defaultDeliveryService: '',
+    backendUrl: '',
+    chainId: '',
+    resolverAddress: '',
+    genomeRegistryAddress: '',
+    showAlways: true,
+    showContacts: true,
+};
+
+describe('useDM3Configuration hook test cases', () => {
+    it('Should initialize the useDM3Configuration hook', () => {
+        const { result } = renderHook(() => useDm3Configuration());
+        const { dm3Configuration, screenWidth, siweValidityStatus } =
+            result.current;
+        expect(dm3Configuration).toStrictEqual(config);
+        expect(siweValidityStatus).toBe(SiweValidityStatus.TO_BE_INITIATED);
+        expect(screenWidth).toBeGreaterThan(0);
+    });
+
+    it('Should set screen width property', async () => {
+        const { result } = renderHook(() => useDm3Configuration());
+        const { setScreenWidth, screenWidth } = result.current;
+        expect(screenWidth).toBe(1024);
+        await act(async () => setScreenWidth(800));
+        await waitFor(() => {
+            const { screenWidth } = result.current;
+            expect(screenWidth).toBe(800);
+        });
+    });
+
+    it('Should update dm3 config property', async () => {
+        const { result } = renderHook(() => useDm3Configuration());
+        const { dm3Configuration, setDm3Configuration } = result.current;
+        expect(dm3Configuration).toStrictEqual(config);
+        dm3Configuration.defaultContact = 'bob.eth';
+        await act(async () => setDm3Configuration(dm3Configuration));
+        await waitFor(() => {
+            const { dm3Configuration } = result.current;
+            expect(dm3Configuration.defaultContact).toBe('bob.eth');
+        });
+    });
+
+    it('Should update SIWE validaty status property', async () => {
+        const { result } = renderHook(() => useDm3Configuration());
+        const { setSiweValidityStatus, siweValidityStatus } = result.current;
+        expect(siweValidityStatus).toStrictEqual(
+            SiweValidityStatus.TO_BE_INITIATED,
+        );
+        await act(async () => setSiweValidityStatus(SiweValidityStatus.ERROR));
+        await waitFor(() => {
+            const { siweValidityStatus } = result.current;
+            expect(siweValidityStatus).toBe(SiweValidityStatus.ERROR);
+        });
+    });
+});
