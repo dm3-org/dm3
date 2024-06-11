5722c37bbaec89c88e864b649ffae7370d09c65e
AlexNi245 <alex.plutta@googlemail.com>
2024-06-11 14:06:37 +0200

    get rid of socket.io dependency in test-helper

--- packages/lib/test-helper/mocks/index.ts
+++ packages/lib/test-helper/mocks/index.ts
@@ -2,7 +2,6 @@ export { getMockDeliveryServiceProfile } from './mockDeliveryServiceProfile';
 export { mockHttpServer } from './mockHttpServer';
 export { MockMessageFactory } from './mockMessageFactory';
 export { mockUserProfile } from './mockUserProfile';
-export { mockWsServer } from './mockWsServer';
 
 export type { MockedUserProfile } from './mockUserProfile';
 export type { MockDeliveryServiceProfile } from './mockDeliveryServiceProfile';
