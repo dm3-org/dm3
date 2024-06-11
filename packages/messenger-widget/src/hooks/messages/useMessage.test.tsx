6f89d76389fca81423dd74167b750a4f21626272
AlexNi245 <alex.plutta@googlemail.com>
2024-06-11 09:43:52 +0200

    dont add message with an empty whitespace

--- packages/messenger-widget/src/hooks/messages/useMessage.test.tsx
+++ packages/messenger-widget/src/hooks/messages/useMessage.test.tsx
@@ -1,23 +1,24 @@
 import '@testing-library/jest-dom';
 import { act, renderHook, waitFor } from '@testing-library/react';
-import { useMessage } from './useMessage';
-import { getMockedStorageContext } from '../../context/testHelper/getMockedStorageContext';
-import { getMockedConversationContext } from '../../context/testHelper/getMockedConversationContext';
-import { getMockedAuthContext } from '../../context/testHelper/getMockedAuthContext';
-import { getMockedTldContext } from '../../context/testHelper/getMockedTldContext';
 import { AuthContext } from '../../context/AuthContext';
-import { TLDContext } from '../../context/TLDContext';
+import { ConversationContext } from '../../context/ConversationContext';
 import { DeliveryServiceContext } from '../../context/DeliveryServiceContext';
 import { StorageContext } from '../../context/StorageContext';
-import { useConversation } from '../conversation/useConversation';
+import { TLDContext } from '../../context/TLDContext';
+import { getMockedAuthContext } from '../../context/testHelper/getMockedAuthContext';
+import { getMockedConversationContext } from '../../context/testHelper/getMockedConversationContext';
 import { getMockedDeliveryServiceContext } from '../../context/testHelper/getMockedDeliveryServiceContext';
-import { ConversationContext } from '../../context/ConversationContext';
-import {
-    DEFAULT_DM3_CONFIGURATION,
-    getMockedDm3Configuration,
-} from '../../context/testHelper/getMockedDm3Configuration';
+import { getMockedStorageContext } from '../../context/testHelper/getMockedStorageContext';
+import { getMockedTldContext } from '../../context/testHelper/getMockedTldContext';
 import { getDefaultContract } from '../../interfaces/utils';
-import { EncryptionEnvelop } from '@dm3-org/dm3-lib-messaging';
+import { useMessage } from './useMessage';
+import {
+    MockMessageFactory,
+    MockedUserProfile,
+    getMockDeliveryServiceProfile,
+    mockUserProfile,
+} from '@dm3-org/dm3-lib-test-helper';
+import { ethers } from 'ethers';
 
 describe('useMessage hook test cases', () => {
     const CONTACT_NAME = 'user.dm3.eth';
@@ -54,16 +55,32 @@ describe('useMessage hook test cases', () => {
     });
 
     describe('add Message', () => {
-        it('should add message from websocket', async () => {
-            const configurationContext = getMockedDm3Configuration({
-                dm3Configuration: {
-                    ...DEFAULT_DM3_CONFIGURATION,
-                    defaultContact: 'mydefaultcontract.eth',
-                },
-            });
+        let sender: MockedUserProfile;
+        let receiver: MockedUserProfile;
+        let ds: any;
 
+        beforeEach(async () => {
+            sender = await mockUserProfile(
+                ethers.Wallet.createRandom(),
+                'alice.eth',
+                ['https://example.com'],
+            );
+            receiver = await mockUserProfile(
+                ethers.Wallet.createRandom(),
+                'bob.eth',
+                ['https://example.com'],
+            );
+            ds = await getMockDeliveryServiceProfile(
+                ethers.Wallet.createRandom(),
+                'https://example.com',
+            );
+        });
+
+        it('should not add empty message', async () => {
             const storageContext = getMockedStorageContext({
                 editMessageBatchAsync: jest.fn(),
+                storeMessageBatch: jest.fn(),
+                storeMessage: jest.fn(),
             });
             const conversationContext = getMockedConversationContext({
                 selectedContact: getDefaultContract('max.eth'),
@@ -104,6 +121,144 @@ describe('useMessage hook test cases', () => {
             await waitFor(() =>
                 expect(result.current.contactIsLoading('max.eth')).toBe(false),
             );
+
+            const messageFactory = MockMessageFactory(
+                sender,
+                receiver,
+                ds.profile,
+            );
+            const message = await messageFactory.createMessage('');
+            const addMessageResult = await waitFor(() =>
+                result.current.addMessage('max.eth', message),
+            );
+
+            expect(addMessageResult).toEqual({
+                isSuccess: false,
+                error: 'Message is empty',
+            });
+        });
+        it('should trim message', async () => {
+            const storageContext = getMockedStorageContext({
+                editMessageBatchAsync: jest.fn(),
+                storeMessageBatch: jest.fn(),
+                storeMessage: jest.fn(),
+            });
+            const conversationContext = getMockedConversationContext({
+                selectedContact: getDefaultContract('max.eth'),
+            });
+            const deliveryServiceContext = getMockedDeliveryServiceContext({
+                //Add websocket mock
+                onNewMessage: (cb: Function) => {
+                    console.log('on new message');
+                },
+                removeOnNewMessageListener: jest.fn(),
+            });
+            const authContext = getMockedAuthContext({});
+            const tldContext = getMockedTldContext({});
+
+            const wrapper = ({ children }: { children: any }) => (
+                <>
+                    <AuthContext.Provider value={authContext}>
+                        <TLDContext.Provider value={tldContext}>
+                            <StorageContext.Provider value={storageContext}>
+                                <ConversationContext.Provider
+                                    value={conversationContext}
+                                >
+                                    <DeliveryServiceContext.Provider
+                                        value={deliveryServiceContext}
+                                    >
+                                        {children}
+                                    </DeliveryServiceContext.Provider>
+                                </ConversationContext.Provider>
+                            </StorageContext.Provider>
+                        </TLDContext.Provider>
+                    </AuthContext.Provider>
+                </>
+            );
+
+            const { result } = renderHook(() => useMessage(), {
+                wrapper,
+            });
+            await waitFor(() =>
+                expect(result.current.contactIsLoading('max.eth')).toBe(false),
+            );
+
+            const messageFactory = MockMessageFactory(
+                sender,
+                receiver,
+                ds.profile,
+            );
+            const message = await messageFactory.createMessage('         ');
+            const addMessageResult = await waitFor(() =>
+                result.current.addMessage('max.eth', message),
+            );
+
+            expect(addMessageResult).toEqual({
+                isSuccess: false,
+                error: 'Message is empty',
+            });
+        });
+        it('should add message', async () => {
+            const storageContext = getMockedStorageContext({
+                editMessageBatchAsync: jest.fn(),
+                storeMessageBatch: jest.fn(),
+                storeMessage: jest.fn(),
+            });
+            const conversationContext = getMockedConversationContext({
+                selectedContact: getDefaultContract('max.eth'),
+            });
+            const deliveryServiceContext = getMockedDeliveryServiceContext({
+                //Add websocket mock
+                onNewMessage: (cb: Function) => {
+                    console.log('on new message');
+                },
+                removeOnNewMessageListener: jest.fn(),
+            });
+            const authContext = getMockedAuthContext({});
+            const tldContext = getMockedTldContext({});
+
+            const wrapper = ({ children }: { children: any }) => (
+                <>
+                    <AuthContext.Provider value={authContext}>
+                        <TLDContext.Provider value={tldContext}>
+                            <StorageContext.Provider value={storageContext}>
+                                <ConversationContext.Provider
+                                    value={conversationContext}
+                                >
+                                    <DeliveryServiceContext.Provider
+                                        value={deliveryServiceContext}
+                                    >
+                                        {children}
+                                    </DeliveryServiceContext.Provider>
+                                </ConversationContext.Provider>
+                            </StorageContext.Provider>
+                        </TLDContext.Provider>
+                    </AuthContext.Provider>
+                </>
+            );
+
+            const { result } = renderHook(() => useMessage(), {
+                wrapper,
+            });
+            await waitFor(() =>
+                expect(result.current.contactIsLoading('max.eth')).toBe(false),
+            );
+
+            const messageFactory = MockMessageFactory(
+                sender,
+                receiver,
+                ds.profile,
+            );
+            const message = await messageFactory.createMessage('hello dm3');
+            const addMessageResult = await waitFor(() =>
+                result.current.addMessage('bob.eth', message),
+            );
+
+            expect(addMessageResult).toEqual({
+                isSuccess: true,
+                error: undefined,
+            });
+            expect(result.current.messages['bob.eth'].length).toBe(1);
         });
     });
 });
