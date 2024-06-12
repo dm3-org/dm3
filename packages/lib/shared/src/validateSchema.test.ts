0d342f4f9ce0c2ee2c3b3b0ed14bd98fd09fb00c
malteish <malteish@corpus.io>
2024-06-12 11:40:25 +0200

    add tests for schema validation

---
+++ packages/lib/shared/src/validateSchema.test.ts
@@ -0,0 +1,79 @@
+import { validateSchema } from './validateSchema';
+
+describe('Schema check', () => {
+    describe('enforce presence', () => {
+        it('Should return true if key is present', async () => {
+            const schema = {
+                type: 'object',
+                properties: {
+                    key0: { type: 'string' },
+                },
+                required: ['key0'],
+                additionalProperties: false,
+            };
+
+            const data = {
+                key0: 'value0',
+            };
+
+            const isValid = validateSchema(schema, data);
+            expect(isValid).toBe(true);
+        });
+        it('Should return false if a required key is missing', async () => {
+            const schema = {
+                type: 'object',
+                properties: {
+                    key0: { type: 'string' },
+                    key1: { type: 'string' },
+                },
+                required: ['key0', 'key1'],
+                additionalProperties: false,
+            };
+
+            const data = {
+                key0: 'value0',
+            };
+
+            const isValid = validateSchema(schema, data);
+            expect(isValid).toBe(false);
+        });
+        it('Should return true if a non-required key is missing', async () => {
+            const schema = {
+                type: 'object',
+                properties: {
+                    key0: { type: 'string' },
+                    key1: { type: 'string' },
+                },
+                required: ['key0'],
+                additionalProperties: false,
+            };
+
+            const data = {
+                key0: 'value0',
+            };
+
+            const isValid = validateSchema(schema, data);
+            expect(isValid).toBe(true);
+        });
+    });
+    describe('enforce absence', () => {
+        it('Should return false if an additional key is present', async () => {
+            const schema = {
+                type: 'object',
+                properties: {
+                    key0: { type: 'string' },
+                },
+                required: ['key0'],
+                additionalProperties: false,
+            };
+
+            const data = {
+                key0: 'value0',
+                key1: 'value1',
+            };
+
+            const isValid = validateSchema(schema, data);
+            expect(isValid).toBe(false);
+        });
+    });
+});
