0cda012caeb42e649b844a285bc6ca819d3be6b5
malteish <malteish@corpus.io>
2024-06-20 13:50:36 +0200

    Revert "add resolver logs"
    This reverts commit 34e3f6b5e73b072c19ea59ec729257918d2e53f8.

--- packages/offchain-resolver/src/http/handleCcipRequest/handler/handleAddr.ts
+++ packages/offchain-resolver/src/http/handleCcipRequest/handler/handleAddr.ts
@@ -6,21 +6,11 @@ import { logDebug } from '@dm3-org/dm3-lib-shared';
 export async function handleAddr(db: IDatabase, request: any) {
     const { name } = request;
 
-    console.log('resolve address for ', name);
-
     const interceptResult = interceptAddr(name);
     logDebug({
         text: '[Interceptor handleAddr]` result ',
         interceptResult,
     });
 
-    if (interceptResult) {
-        console.log('intercepted result for ', name, ' is ', interceptResult);
-    }
-
-    const container = await db.getProfileContainer(name);
-
-    console.log('container for ', name, ' is ', container?.address);
-
-    return interceptResult ?? container?.address;
+    return interceptResult ?? (await db.getProfileContainer(name))?.address;
 }
