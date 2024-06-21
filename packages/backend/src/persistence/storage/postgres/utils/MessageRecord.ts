0816ea34216307b494d7f23274f0eddc3c45f141
AlexNi245 <alex.plutta@googlemail.com>
2024-06-21 09:45:24 +0200

    add docs to MessageRecord

--- packages/backend/src/persistence/storage/postgres/utils/MessageRecord.ts
+++ packages/backend/src/persistence/storage/postgres/utils/MessageRecord.ts
@@ -1,4 +1,10 @@
+//The data model for a message record
 export type MessageRecord = {
+    //Creation time of the message
+    createdAt: number;
+    //The message id. This is the primary key passed by the client.
+    //The client itself is responsible for generating the id because the server is not able to decrypt the message to get the message hash.
     messageId: string;
+    //The actual encrypted message
     encryptedEnvelopContainer: string;
 };
