151a146664c6054ad17cd4d1f409a7857a0b195f
AlexNi245 <alex.plutta@googlemail.com>
2024-07-03 16:21:49 +0200

    remove wagmi import from useMainnetProvider

--- packages/messenger-widget/src/hooks/mainnetprovider/useMainnetProvider.tsx
+++ packages/messenger-widget/src/hooks/mainnetprovider/useMainnetProvider.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { useAccount } from 'wagmi';
 import { MainnetProviderContext } from '../../context/ProviderContext';
 
 export const useMainnetProvider = () => {
-    useAccount({
-        onDisconnect() {
-            console.log('dc');
-        },
-    });
-
     const { provider } = React.useContext(MainnetProviderContext);
     return provider;
 };
