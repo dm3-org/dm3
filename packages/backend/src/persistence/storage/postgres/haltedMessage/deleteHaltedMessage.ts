7c7194f6a10a906b269c8c85f4f17a652021e2a8
AlexNi245 <alex.plutta@googlemail.com>
2024-07-04 12:38:38 +0200

    add deleteHaltedMessage

---
+++ packages/backend/src/persistence/storage/postgres/haltedMessage/deleteHaltedMessage.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+
+export const deleteHaltedMessage =
+    (db: PrismaClient) => async (ensName: string, messageId: string) => {
+        //Find the account first we want to get the messages for
+        const account = await db.account.findFirst({
+            where: {
+                id: ensName,
+            },
+        });
+        //If the contact does not exist, there is no message that can be deleted
+        if (!account) {
+            return false;
+        }
+
+        try {
+            await db.haltedMessage.delete({
+                where: {
+                    id: messageId,
+                },
+            });
+            return true;
+        } catch (e) {
+            console.log('deleteHaltedMessage error', e);
+            return false;
+        }
+    };
