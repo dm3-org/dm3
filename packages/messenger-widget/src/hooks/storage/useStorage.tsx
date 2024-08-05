31b9474713c6736684d5bd16333f0f097cc4cfdf
Bhupesh-MS <mfsi.bhupeshd@gmail.com>
2024-08-05 18:07:19 +0530

    fixed incorrect import

--- packages/messenger-widget/src/hooks/storage/useStorage.tsx
+++ packages/messenger-widget/src/hooks/storage/useStorage.tsx
@@ -15,7 +15,7 @@ import { sha256, stringify } from '@dm3-org/dm3-lib-shared';
 import { Conversation, StorageAPI } from '@dm3-org/dm3-lib-storage';
 import { useEffect, useState } from 'react';
 import { BackendContextType } from '../../context/BackendContext';
-import { HaltedStorageEnvelopContainer } from '../../../../lib/storage/src/new/types';
+import { HaltedStorageEnvelopContainer } from '@dm3-org/dm3-lib-storage';
 
 //Handels storage sync and offers an interface for other hooks to interact with the storage
 export const useStorage = (
