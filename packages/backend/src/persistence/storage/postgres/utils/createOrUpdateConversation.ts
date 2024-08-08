16ded2ba865173b337d343432310dce50e147be8
AlexNi245 <alex.plutta@googlemail.com>
2024-08-08 14:45:23 +0200

    add createOrUpdateConversation db method

--- packages/backend/src/persistence/storage/postgres/utils/createOrUpdateConversation.ts
+++ packages/backend/src/persistence/storage/postgres/utils/createOrUpdateConversation.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
-export const getOrCreateConversation = async (
+export const createOrUpdateConversation = async (
     db: PrismaClient,
     accountId: string,
     encryptedContactName: string,
+    encryptedProfileLocation: string,
 ) => {
     //Check if conversation already exists
     const conversation = await db.conversation.findFirst({
@@ -12,14 +13,26 @@ export const getOrCreateConversation = async (
             encryptedContactName,
         },
     });
-    //If conversation exists, return it
     if (conversation) {
+        //If a conversation already exist. Update the encryptedProfileLocation.
+        //At the moemnt this is the only updatable field
+        await db.conversation.update({
+            where: {
+                id: conversation.id,
+            },
+            data: {
+                encryptedProfileLocation,
+            },
+        });
+
+        //If conversation exists, return it
         return conversation;
     }
     //If conversation does not exist, create it
     return await db.conversation.create({
         data: {
             accountId,
+            encryptedProfileLocation,
             encryptedContactName,
             //Internal field to order conversations properly
             //Will set whenever a conversation is created or a message is added
