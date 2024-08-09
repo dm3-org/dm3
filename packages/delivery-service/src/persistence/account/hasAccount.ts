94f93ae307a00358647894d75b68418015f7f91e
malteish <malteish@corpus.io>
2024-08-09 13:10:47 +0200

    check for address format

--- packages/delivery-service/src/persistence/account/hasAccount.ts
+++ packages/delivery-service/src/persistence/account/hasAccount.ts
@@ -3,6 +3,11 @@ import { ethers } from 'ethers';
 
 export function hasAccount(redis: Redis) {
     return async (address: string) => {
+        if (!ethers.utils.isAddress(address)) {
+            console.debug('hasAccount: Invalid address: ', address);
+            throw Error('hasAccount: Invalid address');
+        }
+
         let account = await redis.get(
             RedisPrefix.Account + ethers.utils.getAddress(address),
         );
