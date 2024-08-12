c4199dd97e084a2f5187d7227f7896eae3f13515
malteish <malteish@corpus.io>
2024-08-12 12:19:36 +0200

    update schema definition

--- packages/lib/delivery/schemas.sh
+++ packages/lib/delivery/schemas.sh
@@ -1,4 +1,4 @@
 yarn ts-json-schema-generator -f tsconfig.json --path Delivery.ts --type DeliveryServiceProperties -o ./src/schema/DeliveryServiceProperties.schema.json --no-type-check \
 && yarn ts-json-schema-generator -f tsconfig.json --path Messages.ts --type Acknowledgment -o ./src/schema/Acknowledgment.schema.json --no-type-check \
-&& yarn ts-json-schema-generator -f tsconfig.json --path Session.ts --type Session -o ./src/schema/Session.schema.json --no-type-check \
+&& yarn ts-json-schema-generator -f tsconfig.json --path Account.ts --type Account -o ./src/schema/Account.schema.json --no-type-check \
 && yarn ts-json-schema-generator -f tsconfig.json --path notifications/types.ts --type NotificationChannel -o ./src/schema/NotificationChannel.schema.json --no-type-check \
