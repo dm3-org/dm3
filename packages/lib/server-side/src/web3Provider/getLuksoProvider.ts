e0c1148997858d8756b0c14d0b9ae61dbe4e5e9c
AlexNi245 <alex.plutta@googlemail.com>
2024-08-29 16:36:24 +0200

    use proper lusko rpc key

--- packages/lib/server-side/src/web3Provider/getLuksoProvider.ts
+++ packages/lib/server-side/src/web3Provider/getLuksoProvider.ts
@@ -12,6 +12,6 @@ export async function getLuksoProvider(
         return key;
     };
 
-    const rpc = readKey('RPC');
+    const rpc = readKey('LUKSO_RPC');
     return new ethers.providers.JsonRpcProvider(rpc);
 }
