c910f9d37cc4ec9bb67cb6db3930c8fa50b21de6
Bhupesh-MS <mfsi.bhupeshd@gmail.com>
2024-10-03 19:08:57 +0530

    added context and feature flag for settings

--- packages/messenger-widget/src/components/Preferences/Settings/Settings.tsx
+++ packages/messenger-widget/src/components/Preferences/Settings/Settings.tsx
@@ -1,18 +1,8 @@
-import { useEffect, useState } from 'react';
-import { Heading } from '../Heading/Heading';
 import './Settings.css';
-
-// message view option type ex : OLD/NEW
-export type MsgViewOptionType = {
-    name: string;
-    viewType: string;
-    isEnabled: boolean;
-};
-
-// settings data type to be used to store in local storage
-export type SettingsDataType = {
-    messageView: string;
-};
+import { useContext } from 'react';
+import { Heading } from '../Heading/Heading';
+import { ModalContext } from '../../../context/ModalContext';
+import { SettingsContext } from '../../../context/SettingsContext';
 
 export function Settings() {
     // heading of the page
@@ -21,129 +11,57 @@ export function Settings() {
     // description of the page
     const description = 'Define how you want to enable/disable components';
 
-    // available message view options
-    const msgViewOptions = [
-        {
-            name: 'Old message view',
-            viewType: 'OLD',
-            isEnabled: true,
-        },
-        {
-            name: 'New message view',
-            viewType: 'NEW',
-            isEnabled: true,
-        },
-    ];
-
-    // state to handle current message view type selected to show in chat screen
-    const [msgViewSelected, setMsgViewSelected] = useState<MsgViewOptionType>(
-        msgViewOptions[0],
-    );
-
-    const getSettingsFromLocalStorage = () => {
-        // fetch settings data from local storage
-        const settingsData = localStorage.getItem('settings');
-        // return data if found else return null
-        return settingsData ? JSON.parse(settingsData) : null;
-    };
-
-    const configureMsgView = () => {
-        // fetch message view type from local storage
-        const settingsData: SettingsDataType | null =
-            getSettingsFromLocalStorage();
-        // if message view found in local storage
-        if (settingsData && settingsData.messageView) {
-            // filter out the message type selected from available options
-            const msgViewType = msgViewOptions.filter(
-                (m) => m.viewType === settingsData.messageView,
-            );
-            // set the selected view type
-            setMsgViewSelected(
-                msgViewType.length ? msgViewType[0] : msgViewOptions[0],
-            );
-        }
-    };
-
-    const updateSettingsInLocalStorage = (data: SettingsDataType) => {
-        // convert JSON data of settings into string
-        const settingsData = JSON.stringify(data);
-        // save the data into local storage
-        localStorage.setItem('settings', settingsData);
-    };
-
-    const updateMsgView = (msgView: MsgViewOptionType) => {
-        // get the view type from selected message view type object
-        const { viewType } = msgView;
-
-        // update local state of message view type selected
-        setMsgViewSelected(msgView);
-
-        // fetch local storage data for settings
-        const settingsData: SettingsDataType | null =
-            getSettingsFromLocalStorage();
-
-        // update the property of message view
-        if (settingsData) {
-            settingsData.messageView = viewType;
-        }
-
-        // create a updated object based on the data
-        const updatedSettings = settingsData
-            ? settingsData
-            : {
-                  messageView: viewType,
-              };
-
-        // update the local storage with the new message view type
-        updateSettingsInLocalStorage(updatedSettings);
-    };
-
-    // on screen load, get the settings data from local storage ad update the states
-    useEffect(() => {
-        configureMsgView();
-    }, []);
+    const { disabledOptions } = useContext(ModalContext);
+    const { msgViewOptions, msgViewSelected, updateMsgView } =
+        useContext(SettingsContext);
 
     return (
         <div>
             {/* heading of the page */}
             <Heading heading={heading} description={description} />
 
-            {/* title and description of the property */}
-            <div className="settings-msg-view">
-                <div className="mt-2 font-weight-800 msg-view-heading">
-                    Message View
-                </div>
-                <div className="font-size-14 msg-view-desc">
-                    Select a view how the message should look like
-                </div>
-            </div>
-
-            {/* radio button options to select the message view type */}
-            <div>
-                <div className="settings-option-container">
-                    {msgViewOptions.map((option, index) => (
-                        <div
-                            key={index}
-                            className="radio d-flex mb-3 width-fit"
-                            onClick={() => {
-                                updateMsgView(option);
-                            }}
-                        >
-                            <input
-                                type="radio"
-                                value={option.viewType}
-                                checked={
-                                    option.viewType === msgViewSelected.viewType
-                                }
-                                readOnly
-                            />
-                            <label className="settings-option radio-label">
-                                {option.name}
-                            </label>
+            {/* Show message option selection when option is not disabled */}
+            {!disabledOptions.settings.messageView && (
+                <>
+                    {/* title and description of the property */}
+                    <div className="settings-msg-view">
+                        <div className="mt-2 font-weight-800 msg-view-heading">
+                            Message View
                         </div>
-                    ))}
-                </div>
-            </div>
+                        <div className="font-size-14 msg-view-desc">
+                            Select a view how the message should look like
+                        </div>
+                    </div>
+
+                    {/* radio button options to select the message view type */}
+                    <div>
+                        <div className="settings-option-container">
+                            {msgViewOptions.map((option, index) => (
+                                <div
+                                    key={index}
+                                    className="radio d-flex mb-3 width-fit"
+                                    onClick={() => {
+                                        updateMsgView(option);
+                                    }}
+                                >
+                                    <input
+                                        type="radio"
+                                        value={option.viewType}
+                                        checked={
+                                            option.viewType ===
+                                            msgViewSelected.viewType
+                                        }
+                                        readOnly
+                                    />
+                                    <label className="settings-option radio-label">
+                                        {option.name}
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
---
+++ packages/messenger-widget/src/hooks/settings/useSettings.tsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from 'react';
+
+// message view option type ex : OLD/NEW
+export type MsgViewOptionType = {
+    name: string;
+    viewType: string;
+    isEnabled: boolean;
+};
+
+// settings data type to be used to store in local storage
+export type SettingsDataType = {
+    messageView: string;
+};
+
+// available message view options
+export const MSG_VIEW_OPTIONS = [
+    {
+        name: 'Old message view',
+        viewType: 'OLD',
+        isEnabled: true,
+    },
+    {
+        name: 'New message view',
+        viewType: 'NEW',
+        isEnabled: true,
+    },
+];
+
+export const useSettings = () => {
+    const msgViewOptions: MsgViewOptionType[] = MSG_VIEW_OPTIONS;
+
+    // state to handle current message view type selected to show in chat screen
+    const [msgViewSelected, setMsgViewSelected] = useState<MsgViewOptionType>(
+        MSG_VIEW_OPTIONS[0],
+    );
+
+    const getSettingsFromLocalStorage = () => {
+        // fetch settings data from local storage
+        const settingsData = localStorage.getItem('settings');
+        // return data if found else return null
+        return settingsData ? JSON.parse(settingsData) : null;
+    };
+
+    const configureMsgView = () => {
+        // fetch message view type from local storage
+        const settingsData: SettingsDataType | null =
+            getSettingsFromLocalStorage();
+        // if message view found in local storage
+        if (settingsData && settingsData.messageView) {
+            // filter out the message type selected from available options
+            const msgViewType = msgViewOptions.filter(
+                (m) => m.viewType === settingsData.messageView,
+            );
+            // set the selected view type
+            setMsgViewSelected(
+                msgViewType.length ? msgViewType[0] : msgViewOptions[0],
+            );
+        }
+    };
+
+    const updateSettingsInLocalStorage = (data: SettingsDataType) => {
+        // convert JSON data of settings into string
+        const settingsData = JSON.stringify(data);
+        // save the data into local storage
+        localStorage.setItem('settings', settingsData);
+    };
+
+    const updateMsgView = (msgView: MsgViewOptionType) => {
+        // get the view type from selected message view type object
+        const { viewType } = msgView;
+
+        // update local state of message view type selected
+        setMsgViewSelected(msgView);
+
+        // fetch local storage data for settings
+        const settingsData: SettingsDataType | null =
+            getSettingsFromLocalStorage();
+
+        // update the property of message view
+        if (settingsData) {
+            settingsData.messageView = viewType;
+        }
+
+        // create a updated object based on the data
+        const updatedSettings = settingsData
+            ? settingsData
+            : {
+                  messageView: viewType,
+              };
+
+        // update the local storage with the new message view type
+        updateSettingsInLocalStorage(updatedSettings);
+    };
+
+    // on screen load, get the settings data from local storage ad update the states
+    useEffect(() => {
+        configureMsgView();
+    }, []);
+
+    return {
+        msgViewSelected,
+        updateMsgView,
+        msgViewOptions,
+    };
+};
